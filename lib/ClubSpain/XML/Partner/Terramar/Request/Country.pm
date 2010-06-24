168337264345163178947838e8203438f5ef75f4
GermanS <german.semenkov@gmail.com>
2010-06-24 19:18:01 +0400

    add simple test for request countries

--- lib/ClubSpain/XML/Partner/Terramar/Request/Country.pm
+++ lib/ClubSpain/XML/Partner/Terramar/Request/Country.pm
@@ -3,4 +3,25 @@ package ClubSpain::XML::Partner::Terramar::Request::Country;
 use strict;
 use warnings;
 
+use XML::LibXML;
+use HTTP::Request;
+
+sub request {
+    my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+    $doc->setStandalone(1);
+    
+    my $root = $doc->createElement('integracion');
+    $root->setAttribute('accion', 'paises');
+
+    my $id_tipo_articulo_clase = $doc->createElement('id_tipo_articulo_clase');
+    $id_tipo_articulo_clase->appendText(1);
+
+    $root->appendChild($id_tipo_articulo_clase);
+    $doc->setDocumentElement($root);
+    
+    return $doc->toString(1);
+}
+
 1;
+
+__END__
--- t/xml/partner/terramar/request/country.t
+++ t/xml/partner/terramar/request/country.t
@@ -1,6 +1,21 @@
-use Test::More tests =>1;
+use Test::More tests =>2;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Partner::Terramar::Request::Country');
+
+my $request_content = <<'';
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="paises">
+  <id_tipo_articulo_clase>1</id_tipo_articulo_clase>
+</integracion>
+
+
+my $content = ClubSpain::XML::Partner::Terramar::Request::Country->request();
+is($content, $request_content, 'check content');
+
+
+
+
+
