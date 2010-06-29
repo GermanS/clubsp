edd0a08607d6d111b02243ee09a067c1e8020262
GermanS <german.semenkov@gmail.com>
2010-06-29 14:20:31 +0400

    add province request

--- lib/ClubSpain/XML/Partner/Terramar/Request/Province.pm
+++ lib/ClubSpain/XML/Partner/Terramar/Request/Province.pm
@@ -3,4 +3,29 @@ package ClubSpain::XML::Partner::Terramar::Request::Province;
 use strict;
 use warnings;
 
+use XML::LibXML;
+
+sub request {
+    my ($class, %params) = @_;
+  
+    my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+    $doc->setStandalone(1);
+    
+    my $root = $doc->createElement('integracion');
+    $root->setAttribute('accion', 'provincias');
+
+    my $id_tipo_articulo_clase = $doc->createElement('id_tipo_articulo_clase');
+    $id_tipo_articulo_clase->appendText(1);
+    
+    my $id_zona = $doc->createElement('id_zona');
+    $id_zona->appendText($params{'zona_id'});
+
+    $root->appendChild($id_tipo_articulo_clase);
+    $root->appendChild($id_zona);
+    
+    $doc->setDocumentElement($root);
+    
+    return $doc->toString(1);
+}
+
 1;
--- t/xml/partner/terramar/request/province.t
+++ t/xml/partner/terramar/request/province.t
@@ -1,6 +1,17 @@
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Partner::Terramar::Request::Province');
+
+my $request_content =<<'';
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="provincias">
+  <id_tipo_articulo_clase>1</id_tipo_articulo_clase>
+  <id_zona>1</id_zona>
+</integracion>
+
+
+my $content = ClubSpain::XML::Partner::Terramar::Request::Province->request( zona_id => 1 );
+is($content, $request_content, 'check content');
