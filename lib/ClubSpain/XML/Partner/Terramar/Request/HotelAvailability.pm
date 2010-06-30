cccf038ae92bf836bf1e69913fa995542173c840
GermanS <german.semenkov@gmail.com>
2010-06-30 15:54:23 +0400

    add hotel availability request

--- lib/ClubSpain/XML/Partner/Terramar/Request/HotelAvailability.pm
+++ lib/ClubSpain/XML/Partner/Terramar/Request/HotelAvailability.pm
@@ -3,4 +3,37 @@ package ClubSpain::XML::Partner::Terramar::Request::HotelAvailability;
 use strict;
 use warnings;
 
+use XML::LibXML;
+
+sub request {
+    my ($class, %params) = @_;
+    
+    my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+    $doc->setStandalone(1);
+    
+    my $root = $doc->createElement('integracion');
+    $root->setAttribute('accion', 'rangosdisponibilidad');
+
+    my $id_articulo = $doc->createElement('id_articulo');
+    $id_articulo->appendText($params{'id_articulo'})
+        if defined $params{'id_articulo'};    
+
+    $root->appendChild($id_articulo);
+    
+    $doc->setDocumentElement($root);
+    
+    return $doc->toString(1); 
+}
+
 1;
+
+__END__
+
+=head 
+
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="rangosdisponibilidad">
+  <id_articulo>101</id_articulo>
+</integracion>
+
+=cut 
--- t/xml/partner/terramar/request/hotelavailability.t
+++ t/xml/partner/terramar/request/hotelavailability.t
@@ -1,6 +1,19 @@
-use Test::More tests =>1;
+use Test::More tests =>2;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Partner::Terramar::Request::HotelAvailability');
+
+my $request_content = <<'';
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="rangosdisponibilidad">
+  <id_articulo>101</id_articulo>
+</integracion>
+
+
+my $content = ClubSpain::XML::Partner::Terramar::Request::HotelAvailability->request(
+    id_articulo => 101
+);
+is($content, $request_content, 'check content');
+
