40d6dd24869737349ccdee5f941b4cba9039f1dc
GermanS <german.semenkov@gmail.com>
2010-06-30 13:48:13 +0400

    impl hotel info request

--- lib/ClubSpain/XML/Partner/Terramar/Request/HotelInfo.pm
+++ lib/ClubSpain/XML/Partner/Terramar/Request/HotelInfo.pm
@@ -3,4 +3,48 @@ package ClubSpain::XML::Partner::Terramar::Request::HotelInfo;
 use strict;
 use warnings;
 
+use XML::LibXML;
+
+sub request {
+    my ($class, %params) = @_;
+    
+    my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+    $doc->setStandalone(1);
+    
+    my $root = $doc->createElement('integracion');
+    $root->setAttribute('accion', 'fichatecnicaprestatario');
+
+    my $id_prestatario = $doc->createElement('id_prestatario');
+    $id_prestatario->appendText($params{'id_prestatario'})
+        if defined $params{'id_prestatario'};
+
+    my $id_idioma = $doc->createElement('id_idioma');
+    $id_idioma->appendText($params{'id_idioma'})
+        if defined $params{'id_idioma'};
+
+    $root->appendChild($id_prestatario);
+    $root->appendChild($id_idioma);
+    
+    $doc->setDocumentElement($root);
+    
+    return $doc->toString(1);
+}
+
 1;
+
+__END__
+
+=head1 request( id_prestatario => , id_idioma => )
+
+Запрос партнеру расширеной информации по отелю
+Форма запроса
+
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="fichatecnicaprestatario">
+  <id_prestatario>1</id_prestatario>
+  <id_idioma>0</id_idioma>
+</integracion>
+
+
+
+=cut
--- t/xml/partner/terramar/request/hotelinfo.t
+++ t/xml/partner/terramar/request/hotelinfo.t
@@ -1,6 +1,19 @@
-use Test::More tests =>1;
+use Test::More tests => 2;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Partner::Terramar::Request::HotelInfo');
+
+my $request_content = <<'';
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="fichatecnicaprestatario">
+  <id_prestatario>100</id_prestatario>
+  <id_idioma>0</id_idioma>
+</integracion>
+
+my $content = ClubSpain::XML::Partner::Terramar::Request::HotelInfo->request(
+    id_prestatario => 100,
+    id_idioma => 0
+);
+is($content, $request_content, 'check content');
