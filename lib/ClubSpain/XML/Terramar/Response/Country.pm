0546c1549b7dc922d6d8e5f545f424f1d32fb765
GermanS <german.semenkov@gmail.com>
2010-07-05 14:36:01 +0400

    add parse() method

--- lib/ClubSpain/XML/Terramar/Response/Country.pm
+++ lib/ClubSpain/XML/Terramar/Response/Country.pm
@@ -3,4 +3,31 @@ package ClubSpain::XML::Terramar::Response::Country;
 use strict;
 use warnings;
 
+use XML::LibXML;
+use ClubSpain::XML::Terramar::Country;
+
+sub parse {
+    my ($self, $xml_sting) = @_;
+    
+    my $dom = XML::LibXML->load_xml( string => $xml_sting );
+    my $root = $dom->getDocumentElement();
+    
+    my $xpath               = new XML::LibXML::XPathExpression('/integracion/pais');    
+    my $xpath_id_zona_pais  = new XML::LibXML::XPathExpression('id_zona_pais');
+    my $xpath_nombre        = new XML::LibXML::XPathExpression('nombre');
+    
+    my @res;
+    my @nodes = $root->findnodes($xpath);
+    foreach my $node (@nodes) {        
+        my $object = ClubSpain::XML::Terramar::Country->new({
+            id_zona_pais => $node->findvalue($xpath_id_zona_pais),
+            nombre       => $node->findvalue($xpath_nombre),
+        });
+       
+        push @res, $object;
+    }
+    
+    return \@res;
+}
+
 1;
--- t/xml/terramar/response_country.t
+++ t/xml/terramar/response_country.t
@@ -1,6 +1,33 @@
-use Test::More tests =>1;
+use Test::More tests => 3;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::Response::Country');
+
+my $response =<<'';
+<?xml version="1.0"?>
+<integracion accion="listado_paises">
+  <pais>
+    <id_zona_pais>3</id_zona_pais>
+    <nombre>Andorra</nombre>
+  </pais>
+  <pais>
+    <id_zona_pais>2</id_zona_pais>
+    <nombre>Portugal</nombre>
+  </pais>
+</integracion>
+
+my $array = ClubSpain::XML::Terramar::Response::Country->parse($response);
+is(scalar @$array, 2, 'count objects');
+
+my $object1 = ClubSpain::XML::Terramar::Country->new({
+    id_zona_pais => 3,
+    nombre       => 'Andorra',
+});
+my $object2 = ClubSpain::XML::Terramar::Country->new({
+    id_zona_pais => 2,
+    nombre       => 'Portugal',
+});
+
+is_deeply($array, [$object1, $object2], 'returned 2 countries');
