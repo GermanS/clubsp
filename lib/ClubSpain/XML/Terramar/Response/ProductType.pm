e8facbe3ce4d351a2f3150aa5778a062455b956d
GermanS <german.semenkov@gmail.com>
2010-07-05 13:52:06 +0400

    add parse product type logic

--- lib/ClubSpain/XML/Terramar/Response/ProductType.pm
+++ lib/ClubSpain/XML/Terramar/Response/ProductType.pm
@@ -3,4 +3,46 @@ package ClubSpain::XML::Terramar::Response::ProductType;
 use strict;
 use warnings;
 
+use XML::LibXML;
+use ClubSpain::XML::Terramar::ProductType;
+
+sub parse {
+    my ( $self, $xml_sting ) = @_;
+    
+    my $dom = XML::LibXML->load_xml( string => $xml_sting );
+    my $root = $dom->getDocumentElement();
+    
+    my $xpath                         = new XML::LibXML::XPathExpression('/integracion/clase');    
+    my $xpath_id                      = new XML::LibXML::XPathExpression('id_tipo_articulo_clase');
+    my $xpath_id_tipo_menu            = new XML::LibXML::XPathExpression('id_tipo_menu');
+    my $xpath_articulo_clase          = new XML::LibXML::XPathExpression('articulo_clase');      
+    my $xpath_busqueda_predeterminada = new XML::LibXML::XPathExpression('busqueda_predeterminada');  
+    my $xpath_zona_predeterminada     = new XML::LibXML::XPathExpression('zona_predeterminada');  
+    my $xpath_regimen_predeterminado  = new XML::LibXML::XPathExpression('regimen_predeterminado');  
+    my $xpath_orden                   = new XML::LibXML::XPathExpression('orden');  
+    my $xpath_visible_minorista       = new XML::LibXML::XPathExpression('visible_minorista');      
+    my $xpath_filtros_busqueda_diferencia_dias = new XML::LibXML::XPathExpression('filtros_busqueda_diferencia_dias');   
+    
+
+    my @res;
+    my @nodes = $root->findnodes($xpath);
+    foreach my $node (@nodes) {        
+        my $obj = ClubSpain::XML::Terramar::ProductType->new({
+            id_tipo_articulo_clase  => $node->findvalue($xpath_id),
+            id_tipo_menu            => $node->findvalue($xpath_id_tipo_menu),
+            articulo_clase          => $node->findvalue($xpath_articulo_clase),
+            busqueda_predeterminada => $node->findvalue($xpath_busqueda_predeterminada),
+            zona_predeterminada     => $node->findvalue($xpath_zona_predeterminada),
+            regimen_predeterminado  => $node->findvalue($xpath_regimen_predeterminado),
+            orden                   => $node->findvalue($xpath_orden),
+            visible_minorista       => $node->findvalue($xpath_visible_minorista),
+            filtros_busqueda_diferencia_dias => $node->findvalue($xpath_filtros_busqueda_diferencia_dias)
+        });
+       
+        push @res, $obj;
+    }
+    
+    return \@res;    
+}
+
 1;
--- t/xml/terramar/response_product_type.t
+++ t/xml/terramar/response_product_type.t
@@ -1,7 +1,61 @@
-use Test::More tests =>1;
+use Test::More tests =>3;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::Response::ProductType');
 
+my $response =<<'';
+<?xml version="1.0"?>
+<integracion accion="listado_clases">
+  <clase>
+    <id_tipo_articulo_clase>1</id_tipo_articulo_clase>
+    <id_tipo_menu>1</id_tipo_menu>
+    <articulo_clase>Hoteles</articulo_clase>
+    <busqueda_predeterminada>Buscar Hotel</busqueda_predeterminada>
+    <zona_predeterminada>Costa del Sol</zona_predeterminada>
+    <regimen_predeterminado>Alojamiento y desayuno</regimen_predeterminado>
+    <orden>4</orden>
+    <visible_minorista>1</visible_minorista>
+    <filtros_busqueda_diferencia_dias>1</filtros_busqueda_diferencia_dias>
+  </clase>
+  <clase>
+    <id_tipo_articulo_clase>7</id_tipo_articulo_clase>
+    <id_tipo_menu>6</id_tipo_menu>
+    <articulo_clase>vacaciones</articulo_clase>
+    <busqueda_predeterminada>Buscar Paquete cerrado</busqueda_predeterminada>
+    <zona_predeterminada/>
+    <regimen_predeterminado/>
+    <orden>8</orden>
+    <visible_minorista>1</visible_minorista>
+    <filtros_busqueda_diferencia_dias>1</filtros_busqueda_diferencia_dias>
+  </clase>
+</integracion>
+
+my $array = ClubSpain::XML::Terramar::Response::ProductType->parse($response);
+is(scalar @$array, 2, 'count objects');
+
+my $class1 = ClubSpain::XML::Terramar::ProductType->new({
+    id_tipo_articulo_clase  => 1,
+    id_tipo_menu            => 1,
+    articulo_clase          => 'Hoteles',
+    busqueda_predeterminada => 'Buscar Hotel',
+    zona_predeterminada     => 'Costa del Sol',
+    regimen_predeterminado  => 'Alojamiento y desayuno',
+    orden                   => 4,
+    visible_minorista       => 1,
+    filtros_busqueda_diferencia_dias => 1,
+});
+my $class2 = ClubSpain::XML::Terramar::ProductType->new({
+    id_tipo_articulo_clase  => 7,
+    id_tipo_menu            => 6,
+    articulo_clase          => 'vacaciones',
+    busqueda_predeterminada => 'Buscar Paquete cerrado',
+    zona_predeterminada     => '',
+    regimen_predeterminado  => '',
+    orden                   => 8,
+    visible_minorista       => 1,
+    filtros_busqueda_diferencia_dias => 1,
+});
+
+is_deeply($array, [$class1, $class2], 'returned 2 classes');
