292d325a9a76684710fa681c6e960267179b8381
GermanS <german.semenkov@gmail.com>
2010-07-05 18:01:02 +0400

    implement parse() method

--- lib/ClubSpain/XML/Terramar/Response/City.pm
+++ lib/ClubSpain/XML/Terramar/Response/City.pm
@@ -1,8 +1,30 @@
-
 package ClubSpain::XML::Terramar::Response::City;
 
 use strict;
 use warnings;
 
-1;
+use XML::LibXML;
+use ClubSpain::XML::Terramar::City;
 
+sub parse {
+    my ($self, $xml_sting) = @_;
+
+    my $dom  = XML::LibXML->load_xml( string => $xml_sting );
+    my $root   = $dom->getDocumentElement();
+    my $xpath  = new XML::LibXML::XPathExpression('/integracion/poblacion');
+
+    my @res;
+    my @nodes = $root->findnodes($xpath);
+    foreach my $node (@nodes) {
+        my $object = ClubSpain::XML::Terramar::City->new({
+            poblacion => $node->textContent()
+        });
+
+        push @res, $object;
+    }
+
+    return \@res;
+}
+
+
+1;
