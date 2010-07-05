2738d8f2a6858f5464281a82ac54867e31f39ab9
GermanS <german.semenkov@gmail.com>
2010-07-05 13:31:21 +0400

    add product type class

--- lib/ClubSpain/XML/Terramar/ProductType.pm
+++ lib/ClubSpain/XML/Terramar/ProductType.pm
@@ -1,3 +1,15 @@
 package ClubSpain::XML::Terramar::ProductType;
 
+use Moose;
+
+has 'id_tipo_articulo_clase'  => ( is => 'rw', required => 1 );
+has 'id_tipo_menu'            => ( is => 'rw', required => 1 );
+has 'articulo_clase'          => ( is => 'rw', required => 1 );
+has 'busqueda_predeterminada' => ( is => 'rw', required => 1 );
+has 'zona_predeterminada'     => ( is => 'rw', required => 1 );
+has 'regimen_predeterminado'  => ( is => 'rw', required => 1 );
+has 'orden'                   => ( is => 'rw', required => 1 );
+has 'visible_minorista'       => ( is => 'rw', required => 1 );
+has 'filtros_busqueda_diferencia_dias' => ( is => 'rw', required => 1 );
+
 1;
--- t/xml/terramar/product_type.t
+++ t/xml/terramar/product_type.t
@@ -4,3 +4,15 @@ use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::ProductType');
+
+my $type = ClubSpain::XML::Terramar::ProductType->new({
+    id_tipo_articulo_clase  => 1,
+    id_tipo_menu            => 2,
+    articulo_clase          => 3,
+    busqueda_predeterminada => 4,
+    zona_predeterminada     => 5,
+    regimen_predeterminado  => 6,
+    orden                   => 7,
+    visible_minorista       => 8,
+    filtros_busqueda_diferencia_dias => 9,
+});
