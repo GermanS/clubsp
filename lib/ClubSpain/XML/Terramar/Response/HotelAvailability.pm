c30afc8b7be611aeb438e81e5fff24f7c0778876
GermanS <german.semenkov@gmail.com>
2010-07-07 20:34:50 +0400

    implement parse() method

--- lib/ClubSpain/XML/Terramar/Response/HotelAvailability.pm
+++ lib/ClubSpain/XML/Terramar/Response/HotelAvailability.pm
@@ -3,4 +3,32 @@ package ClubSpain::XML::Terramar::Response::HotelAvailability;
 use strict;
 use warnings;
 
+use XML::LibXML;
+use ClubSpain::XML::Terramar::HotelAvailability;
+
+sub parse {
+    my ($self, $xml_sting) = @_;
+
+    my $dom  = XML::LibXML->load_xml( string => $xml_sting );
+    my $root   = $dom->getDocumentElement();
+    my $xpath  = new XML::LibXML::XPathExpression('/integracion/intervalo');
+    my $xpath_fecha_desde    = new XML::LibXML::XPathExpression('@fecha_desde');
+    my $xpath_fecha_hasta    = new XML::LibXML::XPathExpression('@fecha_hasta');
+    my $xpath_disponibilidad = new XML::LibXML::XPathExpression('@disponibilidad');
+
+    my @res;
+    my @nodes = $root->findnodes($xpath);
+    foreach my $node (@nodes) {
+        my $object = ClubSpain::XML::Terramar::HotelAvailability->new({
+            fecha_hasta    => $node->findvalue($xpath_fecha_hasta),
+            fecha_desde    => $node->findvalue($xpath_fecha_desde),
+            disponibilidad => $node->findvalue($xpath_disponibilidad),
+        });
+
+        push @res, $object;
+    }
+
+    return \@res;    
+}
+
 1;
--- t/xml/terramar/response_hotel_availability.t
+++ t/xml/terramar/response_hotel_availability.t
@@ -1,6 +1,30 @@
-use Test::More tests => 1;
+use Test::More tests => 3;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::Response::HotelAvailability');
+
+my $response =<<'';
+<?xml version="1.0"?>
+<integracion accion="rangos_disponibilidad">
+  <intervalo fecha_desde="2009-12-11" disponibilidad="0" fecha_hasta="2009-12-24"/>
+  <intervalo fecha_desde="2009-12-25" disponibilidad="1" fecha_hasta="2010-01-19"/>
+</integracion>
+
+
+my $array = ClubSpain::XML::Terramar::Response::HotelAvailability->parse($response);
+is(scalar @$array, 2, 'count objects');
+
+my $object1 = ClubSpain::XML::Terramar::HotelAvailability->new({
+    fecha_desde    => '2009-12-11',
+    fecha_hasta    => '2009-12-24',
+    disponibilidad => 0,
+});
+my $object2 = ClubSpain::XML::Terramar::HotelAvailability->new({
+    fecha_desde    => '2009-12-25',
+    fecha_hasta    => '2010-01-19',
+    disponibilidad => 1,
+});
+
+is_deeply($array, [$object1, $object2], 'returned 2 availability ranges');
