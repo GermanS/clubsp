ebde61fbed7af1c777141e17b771e7e937f72d03
GermanS <german.semenkov@gmail.com>
2010-07-07 18:36:07 +0400

    implement request() method

--- lib/ClubSpain/XML/Terramar/Request/Occupation.pm
+++ lib/ClubSpain/XML/Terramar/Request/Occupation.pm
@@ -1,6 +1,44 @@
-package ClubSpain::XML::Terramar::Occupation;
+package ClubSpain::XML::Terramar::Request::Occupation;
 
 use strict;
 use warnings;
+
+use XML::LibXML;
+
+sub request {
+    my ($class, %params) = @_;
+  
+    my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+    $doc->setStandalone(1);
+    
+    my $root = $doc->createElement('integracion');
+    $root->setAttribute('accion', 'ocupaciones');
+
+    my $id_tipo_articulo_clase = $doc->createElement('id_tipo_articulo_clase');
+    $id_tipo_articulo_clase->appendText($params{'id_tipo_articulo_clase'})
+        if defined $params{'id_tipo_articulo_clase'};
+    
+    $root->appendChild($id_tipo_articulo_clase);
+    
+    $doc->setDocumentElement($root);
+    
+    return $doc->toString(1);
+}
+
+1;
+
+__END__
+
+=head1 request( id_tipo_articulo_clase =>  )
+
+Запрос партнеру максимальногого и минимального количества туристов
+Форма запроса
+
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="ocupaciones">
+  <id_tipo_articulo_clase>99</id_tipo_articulo_clase>
+</integracion>
+
+=cut
 
 1;
--- t/xml/terramar/request_occupation.t
+++ t/xml/terramar/request_occupation.t
@@ -1,6 +1,17 @@
-use Test::More tests =>1;
+use Test::More tests =>2;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::Request::Occupation');
+
+my $request_content = <<'';
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<integracion accion="ocupaciones">
+  <id_tipo_articulo_clase>99</id_tipo_articulo_clase>
+</integracion>
+
+my $content = ClubSpain::XML::Terramar::Request::Occupation->request( 
+    id_tipo_articulo_clase => 99,
+);
+is($content, $request_content, 'check content');
