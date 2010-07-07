fe030412e7079e3686eae4579840315b9889a9df
GermanS <german.semenkov@gmail.com>
2010-07-07 18:46:27 +0400

    add tests

--- lib/ClubSpain/XML/Terramar/Occupation.pm
+++ lib/ClubSpain/XML/Terramar/Occupation.pm
@@ -2,4 +2,10 @@ package ClubSpain::XML::Terramar::Occupation;
 
 use Moose;
 
+has 'adultos_max'   => ( is => 'rw', required => 1 );
+has 'adultos_min'   => ( is => 'rw', required => 1 );
+has 'ninos_max'     => ( is => 'rw', required => 1 );
+has 'pax_max'       => ( is => 'rw', required => 1 );
+has 'edad_nino_max' => ( is => 'rw', required => 1 );
+
 1;
