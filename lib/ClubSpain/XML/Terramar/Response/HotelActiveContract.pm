fb8931678e595a30838eeadb354ec93cf45cc409
GermanS <german.semenkov@gmail.com>
2010-07-09 20:20:02 +0400

    implement parse() method

--- lib/ClubSpain/XML/Terramar/Response/HotelActiveContract.pm
+++ lib/ClubSpain/XML/Terramar/Response/HotelActiveContract.pm
@@ -3,4 +3,39 @@ package ClubSpain::XML::Terramar::Response::HotelActiveContract;
 use strict;
 use warnings;
 
+use XML::LibXML;
+use ClubSpain::XML::Terramar::HotelActiveContract;
+
+sub parse {
+    my ($self, $xml_sting) = @_;
+
+    my $dom  = XML::LibXML->load_xml( string => $xml_sting );
+    my $root = $dom->getDocumentElement();
+
+    my $xpath                               = new XML::LibXML::XPathExpression('/integracion/articulo');
+    my $xpath_id                            = new XML::LibXML::XPathExpression('id');
+    my $xpath_nombre                        = new XML::LibXML::XPathExpression('nombre');
+    my $xpath_id_tipo_articulo_clase        = new XML::LibXML::XPathExpression('id_tipo_articulo_clase');
+    my $xpath_id_tipo_articulo_superclase   = new XML::LibXML::XPathExpression('id_tipo_articulo_superclase');
+    my $xpath_id_zona_pais                  = new XML::LibXML::XPathExpression('id_zona_pais');
+    my $xpath_id_zona                       = new XML::LibXML::XPathExpression('id_zona');
+
+    my @res;
+    my @nodes = $root->findnodes($xpath);
+    foreach my $node (@nodes) {
+        my $object = ClubSpain::XML::Terramar::HotelActiveContract->new({
+            id                          => $node->findvalue($xpath_id),
+            nombre                      => $node->findvalue($xpath_nombre),
+            id_tipo_articulo_clase      => $node->findvalue($xpath_id_tipo_articulo_clase),
+            id_tipo_articulo_superclase => $node->findvalue($xpath_id_tipo_articulo_superclase),
+            id_zona_pais                => $node->findvalue($xpath_id_zona_pais),
+            id_zona                     => $node->findvalue($xpath_id_zona),
+        });
+
+        push @res, $object;
+    }
+
+    return \@res;
+}
+
 1;
--- t/xml/terramar/response_hotel_active_contract.t
+++ t/xml/terramar/response_hotel_active_contract.t
@@ -1,6 +1,34 @@
-use Test::More tests => 1;
+use Test::More tests => 3;
 
 use strict;
 use warnings;
 
 use_ok('ClubSpain::XML::Terramar::Response::HotelActiveContract');
+
+my $response =<<'';
+<?xml version="1.0"?>
+<integracion accion="listado_articulos_activos">
+  <articulo>
+    <id>2911</id>
+    <nombre>14 noches</nombre>
+    <id_tipo_articulo_clase>67</id_tipo_articulo_clase>
+    <id_tipo_articulo_superclase>17</id_tipo_articulo_superclase>
+    <id_zona_pais>1</id_zona_pais>
+    <id_zona>2</id_zona>
+  </articulo>
+</integracion>
+
+
+my $array = ClubSpain::XML::Terramar::Response::HotelActiveContract->parse($response);
+is(scalar @$array, 1, 'count objects');
+
+my $contract = ClubSpain::XML::Terramar::HotelActiveContract->new({
+    id                          => 2911,
+    nombre                      => '14 noches',
+    id_tipo_articulo_clase      => 67,
+    id_tipo_articulo_superclase => 17,
+    id_zona_pais                => 1,
+    id_zona                     => 2,
+});
+
+is_deeply($array, [$contract], 'returned contract');
