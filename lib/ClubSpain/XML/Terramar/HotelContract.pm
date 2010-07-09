794d484848e8cbd93641fb952fc5fddba572b05b
GermanS <german.semenkov@gmail.com>
2010-07-09 12:33:55 +0400

    cosmetics

--- lib/ClubSpain/XML/Terramar/HotelContract.pm
+++ lib/ClubSpain/XML/Terramar/HotelContract.pm
@@ -61,26 +61,48 @@ has 'suplemento'    => ( is => 'rw', required => 1 );
 =head
 
 <integracion accion="informe_imprenta" id_articulo="848" prestatario="49" nombre_articulo="14 noches">
-        <ocupacion min_adultos="1" max_adultos="3" max_ninos="3" min_pax="1" max_pax="4"/>
-        <rango fecha_desde="2009-11-02" fecha_hasta="2009-11-08">
-        <precios>
-                 <precio con_suplemento_de_regimen="PC" importe="479.00" importe_por="pax"/>
-        </precios>
-        <suplementos>
-                 <suplemento id_suplemento="1086" fecha_desde="2009-11-02" fecha_hasta="2010-03-22"
-                 pvp="210.00" porcentaje="0.00" id_clase="2" nombre_clase="Extra pax" referencia_pb="1"
-                 obligatorio="0" importe_por="pax" rango_fechas="total">Individual</suplemento>
-                 <suplemento id_suplemento="1087" fecha_desde="2009-11-02" fecha_hasta="2010-03-22"
-                 pvp="0.00" porcentaje="0.00" id_clase="4" nombre_clase="Varios" referencia_pb="1"
-                 obligatorio="1" importe_por="articulo" rango_fechas="total">
-                 BONIFICACION 100 EUROS POR PASAJERO
-                 </suplemento>
-                 <suplemento id_suplemento="1088" fecha_desde="2009-11-02" fecha_hasta="2010-03-22"
-                 pvp="0.00" porcentaje="0.00" id_clase="2" nombre_clase="Extra pax" referencia_pb="1"
-                 obligatorio="0" importe_por="pax" rango_fechas="total">Tercera persona</suplemento>
-        </suplementos>
-        <ofertas/>
-        </rango>
+  <ocupacion min_adultos="1" max_adultos="3" max_ninos="3" min_pax="1" max_pax="4"/>
+  <rango fecha_desde="2009-11-02" fecha_hasta="2009-11-08">
+    <precios>
+      <precio con_suplemento_de_regimen="PC" importe="479.00" importe_por="pax"/>
+    </precios>
+    <suplementos>
+      <suplemento id_suplemento="1086" 
+                  fecha_desde="2009-11-02" 
+                  fecha_hasta="2010-03-22"
+                  pvp="210.00" 
+                  porcentaje="0.00" 
+                  id_clase="2" 
+                  nombre_clase="Extra pax" 
+                  referencia_pb="1"
+                  obligatorio="0" 
+                  importe_por="pax" 
+                  rango_fechas="total">Individual</suplemento>
+      <suplemento id_suplemento="1087"
+                  fecha_desde="2009-11-02"
+                  fecha_hasta="2010-03-22"
+                  pvp="0.00" 
+                  porcentaje="0.00" 
+                  id_clase="4" 
+                  nombre_clase="Varios" 
+                  referencia_pb="1"
+                  obligatorio="1" 
+                  importe_por="articulo" 
+                  rango_fechas="total">BONIFICACION 100 EUROS POR PASAJERO</suplemento>
+      <suplemento id_suplemento="1088" 
+                  fecha_desde="2009-11-02" 
+                  fecha_hasta="2010-03-22"
+                  pvp="0.00" 
+                  porcentaje="0.00" 
+                  id_clase="2" 
+                  nombre_clase="Extra pax" 
+                  referencia_pb="1"
+                  obligatorio="0" 
+                  importe_por="pax" 
+                  rango_fechas="total">Tercera persona</suplemento>
+    </suplementos>
+    <ofertas/>
+  </rango>
 </integracion>
 
 =cut
