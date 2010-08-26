a3f36f5f7e0cedf48c8b5cf6152bf5fdab1bd0de
GermanS <german.semenkov@gmail.com>
2010-08-26 19:14:44 +0400

    implement low level stub for database

--- lib/ClubSpain/Model/FlightManager.pm
+++ lib/ClubSpain/Model/FlightManager.pm
@@ -3,24 +3,121 @@ package ClubSpain::Model::FlightManager;
 use strict;
 use warnings;
 
-my $russia = new ClubSpain::Model::Country({ name => 'Россия',  code => 'ru' });
-my $mow    = new ClubSpain::Model::City({ name => 'Москва', code => 'mow' });
+my $russia = new ClubSpain::Model::Country({ id => 1, name => 'Россия',  code => 'ru' });
+my $spain  = new ClubSpain::Model::Country({ id => 2, name => 'Испания', code => 'es' });
 
-my $spain = new ClubSpain::Model::Country({ name => 'Испания', code => 'es' });
-my $bcn   = new ClubSpain::Model::City({ code => 'bcn', name => 'Барселона' });
-my $mal   = new ClubSpain::Model::City({ code => 'mal', name => 'Малага' });
+my $mow = new ClubSpain::Model::City({ id => 1, code => 'mow', name => 'Москва' });
+my $bcn = new ClubSpain::Model::City({ id => 2, code => 'bcn', name => 'Барселона' });
+my $mal = new ClubSpain::Model::City({ id => 3, code => 'mal', name => 'Малага' });
 
-sub departure_countries {
+my $departure = {
+    1 => {
+        2 => [
+            new ClubSpain::Model::Date({ id => '04-09-2010', name => '04-09-2010'}),
+            new ClubSpain::Model::Date({ id => '11-09-2010', name => '11-09-2010'}),
+            new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+            new ClubSpain::Model::Date({ id => '25-09-2010', name => '25-09-2010'}),
+        ],
+        3 => [
+            new ClubSpain::Model::Date({ id => '12-10-2010', name => '12-10-2010'}),
+            new ClubSpain::Model::Date({ id => '19-10-2010', name => '19-10-2010'}),
+            new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+        ]
+    },
+    2  => {
+        1 => [
+            new ClubSpain::Model::Date({ id => '04-09-2010', name => '04-09-2010'}),
+            new ClubSpain::Model::Date({ id => '11-09-2010', name => '11-09-2010'}),
+            new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+            new ClubSpain::Model::Date({ id => '25-09-2010', name => '25-09-2010'}),
+        ]
+    },
+    3 => {
+        1 => [
+            new ClubSpain::Model::Date({ id => '12-10-2010', name => '12-10-2010'}),
+            new ClubSpain::Model::Date({ id => '19-10-2010', name => '19-10-2010'}),
+            new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+        ]
+    }
+};
+
+my $return = {
+    2 => {
+        1 => {
+            '04-09-2010' => [
+                new ClubSpain::Model::Date({ id => '11-09-2010', name => '11-09-2010'}),
+                new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+            ],
+            '11-09-2010' => [
+                new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+                new ClubSpain::Model::Date({ id => '25-09-2010', name => '15-09-2010'}),
+            ],
+            '18-09-2010' => [
+                new ClubSpain::Model::Date({ id => '25-09-2010', name => '25-09-2010'}),
+            ],
+            '25-09-2010' => [
+                new ClubSpain::Model::Date({ id => '03-10-2010', name => '03-10-2010'}),
+            ]
+        }
+    },
+    3 => {
+        1 => {
+            '12-10-2010' => [
+                new ClubSpain::Model::Date({ id => '19-10-2010', name => '19-10-2010'}),
+                new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+            ],
+            '19-10-2010' => [
+                new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+            ],
+            '26-10-2010' => [
+                new ClubSpain::Model::Date({ id => '03-11-2010', name => '03-11-2010'}),
+                new ClubSpain::Model::Date({ id => '10-11-2010', name => '10-11-2010'}),
+                new ClubSpain::Model::Date({ id => '17-11-2010', name => '17-11-2010'}),
+            ]
+        }
+    },
+    1 => {
+        2 => {
+            '04-09-2010' => [
+                new ClubSpain::Model::Date({ id => '11-09-2010', name => '11-09-2010'}),
+                new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+                new ClubSpain::Model::Date({ id => '25-09-2010', name => '25-09-2010'}),
+            ],
+            '11-09-2010' => [
+                new ClubSpain::Model::Date({ id => '18-09-2010', name => '18-09-2010'}),
+                new ClubSpain::Model::Date({ id => '25-09-2010', name => '25-09-2010'}),
+            ],
+            '25-09-2010' => [
+                new ClubSpain::Model::Date({ id => '02-10-2010', name => '02-10-2010'}),
+            ]
+        },
+        3 => {
+            '12-10-2010' => [
+                new ClubSpain::Model::Date({ id => '19-10-2010', name => '19-10-2010'}),
+                new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+            ],
+            '19-10-2010' => [
+                new ClubSpain::Model::Date({ id => '26-10-2010', name => '26-10-2010'}),
+            ],
+            '26-10-2010' => [
+                new ClubSpain::Model::Date({ id => '10-11-2010', name => '10-11-2010'}),
+                new ClubSpain::Model::Date({ id => '17-11-2010', name => '17-11-2010'}),
+            ]
+        }
+    }
+};
+
+sub departing_country {
     my $class = shift;
 
-    return [ $russia, $spain ]
+    return [ $russia, $spain ];
 }
 
-sub departure_cities {
-    my ($class, $country) = shift;
+sub departing_city {
+    my ($class, $country) = @_;
 
     my @res;
-    if ($country && $country eq 'es') {
+    if ($country && $country == '2') {
         push @res, $bcn, $mal;
     } else {
         push @res, $mow;
@@ -29,24 +126,24 @@ sub departure_cities {
     return \@res;
 }
 
-sub arrival_countries {
-    my ($class, $departure) = @_;
+sub destination_country {
+    my ($class, $city) = @_;
 
     my @res;
-    if ($departure && $departure eq 'es') {
-        push @res, $russia;
-    } else {
+    if ($city && $city == 1) {
         push @res, $spain;
+    } else {
+        push @res, $russia;
     }
 
     return \@res;
 }
 
-sub arrival_cities {
-    my ($class, $arrival) = @_;
+sub destination_city {
+    my ($class, $destination_country) = @_;
 
     my @res;
-    if ($arrival && $arrival eq 'ru') {
+    if ($destination_country && $destination_country == '1') {
         push @res, $mow;
     } else  {
         push @res, $bcn, $mal;
@@ -55,10 +152,16 @@ sub arrival_cities {
     return \@res;
 }
 
-sub departure_dates {
+sub departing_date {
+    my ($class, $params) = @_;
+
+    return $departure->{$params->{'departing_city'}}{$params->{'destination_city'}};
 }
 
-sub arrival_dates {
+sub returning_date {
+    my ($class, $params) = @_;
+
+    return $return->{$params->{'departing_city'}}{$params->{'destination_city'}}{$params->{'departing_date'}};
 }
 
 sub fare {
