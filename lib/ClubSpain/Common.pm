43ac402b2654cdb543d67adc12c1134bbcb5693a
GermanS <german.semenkov@gmail.com>
2010-11-10 15:47:47 +0300

    initial insert

---
+++ lib/ClubSpain/Common.pm
@@ -0,0 +1,27 @@
+package ClubSpain::Common;
+
+use strict;
+use warnings;
+
+use vars qw(@EXPORT_OK %EXPORT_TAGS);
+
+BEGIN {
+    use base qw(Exporter);
+};
+
+@EXPORT_OK = qw(&minify);
+
+%EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+sub minify {
+    my $value = shift;
+    return unless $value;
+
+    $value =~ s/^\s+//;
+    $value =~ s/\s+$//;
+    $value =~ s/\s{2,}/ /g;
+
+    return $value;
+}
+
+1;
