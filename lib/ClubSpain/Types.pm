3c5d3b0ded57d0daabf66c3fddef1d56f331d346
GermanS <german.semenkov@gmail.com>
2010-11-10 15:46:47 +0300

    minor changes in messages

--- lib/ClubSpain/Types.pm
+++ lib/ClubSpain/Types.pm
@@ -11,7 +11,7 @@ subtype 'Natural'
     => where { $_ > 0 }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "This number $_ is not a positive integer!"
+            message => sprintf "This number %s is not a positive integer!", $_
         )
     };
 
@@ -20,43 +20,43 @@ subtype 'NaturalLessThan1000'
     => where { $_ < 1000 }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "This number $_ is not less than 1000!"
+            message => sprintf "This number %s is not less than 1000!", $_
         )
     };
 
 subtype 'AlphaLength1'
     => as 'Str'
-    => where { $_ =~ /^[a-z]{1}$/i }
+    => where { $_ && $_ =~ /^[a-z]{1}$/i }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "The $_ is not 1 char!"
+            message => sprintf "The %s is not 1 char!", $_
         )
     };
 
 subtype 'AlphaLength2'
     => as 'Str'
-    => where { $_ =~ /^[a-z]{2}$/i }
+    => where { $_ && $_ =~ /^[a-z]{2}$/i }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "The $_ is not 2 chars word!"
+            message => sprintf "The %s is not 2 chars word!", $_
         )
     };
 
 subtype 'AlphaLength3'
     => as 'Str'
-    => where { $_ =~ /^[a-z]{3}$/i }
+    => where { $_ && $_ =~ /^[a-z]{3}$/i }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "The $_ is not 3 chars word!"
+            message => sprintf "The %s is not 3 chars word!", $_
         )
     };
 
 subtype 'StringLength2to255'
     => as 'Str'
-    => where { $_ =~ /^.{2,255}$/ }
+    => where { $_ && $_ =~ /^.{2,255}$/ }
     => message {
         throw ClubSpain::Exception::Validation(
-            message => "The $_ is less than 2 or more than 255 chars"
+            message => sprintf "The %s is less than 2 or more than 255 chars", $_
         )
     };
 
