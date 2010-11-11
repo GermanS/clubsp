e5af76991ec193409ebeee3b8e8b09199e4351bf
GermanS <german.semenkov@gmail.com>
2010-11-11 14:58:28 +0300

    implement create()

--- lib/ClubSpain/Design/Country.pm
+++ lib/ClubSpain/Design/Country.pm
@@ -3,10 +3,9 @@ package ClubSpain::Design::Country;
 use Moose;
 use namespace::autoclean;
 
-use parent qw(ClubSpain::Design::Base);
-
-use ClubSpain::Types;
 use ClubSpain::Common qw(minify);
+use ClubSpain::Storage;
+use ClubSpain::Types;
 
 has 'id'       => ( is => 'ro' );
 has 'name'     => ( is => 'ro', required => 1, isa => 'StringLength2to255' );
@@ -20,6 +19,31 @@ sub BUILDARGS {
     $param{'name'} = minify($param{'name'});
 
     return \%param;
+}
+
+sub create {
+    my $self = shift;
+
+    my $schema = ClubSpain::Storage->instance()->schema();
+
+    $schema->resultset('Country')->create({
+        name        => $self->name,
+        alpha2      => $self->alpha2,
+        alpha3      => $self->alpha3,
+        numerics    => $self->numerics,
+    });
+}
+
+sub update {
+}
+
+sub delete {
+}
+
+sub retrieve {
+}
+
+sub search {
 }
 
 __PACKAGE__->meta->make_immutable();
