8c1c7cb1e6557326de8884f24f5465cec9c99b46
GermanS <german.semenkov@gmail.com>
2010-11-17 13:38:22 +0300

    initial insert

---
+++ lib/ClubSpain/Controller/BackOffice.pm
@@ -0,0 +1,18 @@
+package ClubSpain::Controller::BackOffice;
+
+use strict;
+use warnings;
+
+use parent qw(Catalyst::Controller);
+
+# base sub for matching public path /account
+# -- this verifies logged-in-ness for everything chained onto it
+sub base :Chained("/") :PathPart('backoffice') :CaptureArgs(0) {
+}
+
+
+# display /backoffice page
+sub root :Chained("base") :PathPart("") :Args(0) {
+}
+
+1;
