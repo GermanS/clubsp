9da54891f976032270a88d4f48aaa1dd9f159d52
GermanS <german.semenkov@gmail.com>
2010-11-26 13:12:35 +0300

    use utf8

--- lib/ClubSpain/View/TT.pm
+++ lib/ClubSpain/View/TT.pm
@@ -2,12 +2,13 @@ package ClubSpain::View::TT;
 
 use strict;
 use warnings;
+use utf8;
 
 use parent qw(Catalyst::View::TT);
 
 __PACKAGE__->config(
-    ENCODING            => 'utf-8',
-    TEMPLATE_EXTENSIOn  => 'tt2',
+    ENCODING            => 'UTF-8',
+    TEMPLATE_EXTENSION  => 'tt2',
     PRE_CHOMP           => 2,
     POST_CHOMP          => 2,
     INCLUDE_PATH        => [ ClubSpain->path_to('root', 'base') ]
