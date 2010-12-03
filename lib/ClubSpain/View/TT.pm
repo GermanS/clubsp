b08eeb28292b0ee9041aab912c7bc2bb2e3e60c0
GermanS <german.semenkov@gmail.com>
2010-12-03 16:54:16 +0300

    add wiki filter via Text::GooglewikiFormat

--- lib/ClubSpain/View/TT.pm
+++ lib/ClubSpain/View/TT.pm
@@ -6,12 +6,22 @@ use utf8;
 
 use parent qw(Catalyst::View::TT);
 
+use Text::GooglewikiFormat;
+
 __PACKAGE__->config(
     ENCODING            => 'UTF-8',
     TEMPLATE_EXTENSION  => 'tt2',
     PRE_CHOMP           => 2,
     POST_CHOMP          => 2,
-    INCLUDE_PATH        => [ ClubSpain->path_to('root', 'base') ]
+    INCLUDE_PATH        => [ ClubSpain->path_to('root', 'base') ],
+    FILTERS => {
+        'wiki' => sub {
+            my $text  = shift;
+
+            my %tags = %Text::GooglewikiFormat::tags;
+            return Text::GooglewikiFormat::format( $text );
+        },
+    }
 );
 
 1;
