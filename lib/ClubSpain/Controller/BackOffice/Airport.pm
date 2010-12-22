1f17dabda8d947e906cd4c1c82266874357637fc
GermanS <german.semenkov@gmail.com>
2010-12-22 15:43:42 +0300

    inital insert

---
+++ lib/ClubSpain/Controller/BackOffice/Airport.pm
@@ -0,0 +1,219 @@
+package ClubSpain::Controller::BackOffice::Airport;
+use strict;
+use warnings;
+use utf8;
+
+use parent qw(Catalyst::Controller::HTML::FormFu);
+use ClubSpain::Constants qw(:all);
+use ClubSpain::Design::Country;
+use ClubSpain::Design::City;
+use ClubSpain::Design::Airport;
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $c->stash(
+        template     => 'backoffice/airport.tt2',
+        country_list => ClubSpain::Design::Country->list()
+    );
+};
+
+sub default :Path {};
+
+sub end :ActionClass('RenderView') {};
+
+sub base :Chained('/backoffice/base') :PathPart('airport') :CaptureArgs(0) {};
+
+sub id :Chained('base') :PathPart('') :CaptureArgs(1) {
+    my ($self, $c, $id) = @_;
+
+    my $port;
+    eval {
+        $port = ClubSpain::Design::Airport->fetch_by_id($id);
+        $c->stash( port => $port );
+    };
+
+    if ($@) {
+        $self->process_error($c, $@) if $@;
+        $c->response->redirect($c->uri_for('default'));
+        $c->detach();
+    }
+};
+
+sub enable :Chained('id') :PathPart('enable') :Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash->{'port'}->update({
+        is_published => ENABLE
+    });
+    $c->res->redirect($c->uri_for('browse', $c->stash->{'port'}->city_id));
+};
+
+sub disable :Chained('id') :PathPart('disable') :Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash->{'port'}->update({
+        is_published => DISABLE
+    });
+    $c->res->redirect($c->uri_for('browse', $c->stash->{'port'}->city_id));
+};
+
+sub delete :Chained('id') :PathPart('delete') :Args(0) {
+    my ($self, $c) = @_;
+
+    eval {
+        ClubSpain::Design::City->delete($c->stash->{'port'}->id);
+    };
+
+    if ($@) {
+        $self->process_error($c, $@);
+    } else {
+        $self->successful_message($c);
+    }
+
+    $c->res->redirect($c->uri_for('browse', $c->stash->{'port'}->city_id));
+};
+
+sub process_error {
+    my ($self, $c, $e) = @_;
+
+    if ($e = Exception::Class->caught('ClubSpain::Exception::Validation')) {
+        $c->stash( message => $e->message );
+    } elsif ($e = Exception::Class->caught('ClubSpain::Exception::Storage')) {
+        $c->stash( message => $e->message );
+    } else {
+        $c->stash( message => $@ );
+    }
+};
+
+sub successful_message {
+    my ($self, $c) = @_;
+
+    $c->stash( message => MESSAGE_OK );
+};
+
+sub load_add_form :Private  {
+    my $self = shift;
+
+    my $form = $self->form();
+    $form->load_config_filestem('backoffice/airport_form');
+    $form->process();
+
+    return $form;
+};
+
+sub create :Local {
+    my ($self, $c) = @_;
+
+    $c->stash->{'current_model_instance'} =
+        ClubSpain::Design::City->schema()->resultset('City');
+
+    my $form = $self->load_add_form();
+    if ($form->submitted_and_valid()) {
+        $self->insert($c);
+    }
+
+    $c->stash(
+        form    => $self->load_add_form(),
+        template => 'backoffice/airport_form.tt2'
+    );
+};
+
+sub insert :Private {
+    my ($self, $c) = @_;
+
+    eval {
+        my $port = ClubSpain::Design::Airport->new(
+            city_id  => $c->request->param('city_id'),
+            iata     => $c->request->param('iata'),
+            icoa     => $c->request->param('icao'),
+            name     => $c->request->param('name'),
+            is_published => ENABLE,
+        );
+        $port->create();
+
+        $self->successful_message($c);
+    };
+
+    $self->process_error($c, $@)
+        if $@;
+};
+
+sub load_upd_form :Private {
+    my ($self, $c) = @_;
+
+    my $form = $self->load_add_form();
+    my $port = $c->stash->{'port'};
+
+    $form->get_element({ name => 'city_id' })->value($port->city_id);
+    $form->get_element({ name => 'iata' })->value($port->iata);
+    $form->get_element({ name => 'icao' })->value($port->icao);
+    $form->get_element({ name => 'name' })->value($port->name);
+
+    $form->process;
+
+    return $form;
+};
+
+sub edit :Chained('id') :PathPart('edit') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $form = $self->load_upd_form($c);
+    if ($form->submitted_and_valid()) {
+        $self->update($c);
+    }
+
+    $c->stash(
+        form => $self->load_upd_form($c),
+        template => 'backoffice/airport_form.tt2'
+    );
+};
+
+sub update :Private {
+    my ($self, $c) = @_;
+
+    eval {
+        my $port = ClubSpain::Design::Port->new(
+            id          => $c->stash->{'port'}->id,
+            city_id     => $c->request->param('city_id'),
+            icao        => $c->request->param('iaco'),
+            iata        => $c->request->param('iata'),
+            name        => $c->request->param('name'),
+            is_published=> $c->stash->{'port'}->is_published,
+        );
+        $port->update();
+
+        $self->successful_message($c);
+    };
+
+    $self->process_error($c, $@)
+         if $@;
+};
+
+sub browse :Local :Args(1) {
+    my ($self, $c, $country) = @_;
+
+    $c->stash(
+        iterator => ClubSpain::Design::City->list({country_id => $country})
+    );
+}
+
+sub country :Local :Args(1) {
+    my ($self, $c, $country_id) = @_;
+
+    $c->stash(
+        selected_country => ClubSpain::Design::Country->fetch_by_id($country_id)
+    );
+}
+
+sub city :Local :Args(1) {
+    my ($self, $c, $city_id) = @_;
+
+    my $city = ClubSpain::Design::City->fetch_by_id($city_id);
+    $c->stash( selected_city => $city );
+
+    $self->country($c, $city->country_id);
+}
+
+
+1;
