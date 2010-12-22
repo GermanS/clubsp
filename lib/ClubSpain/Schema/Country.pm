999e6c3aab2ed231bc21cc4499e1205a5980c0f1
GermanS <german.semenkov@gmail.com>
2010-12-22 15:41:59 +0300

    rename relationship

--- lib/ClubSpain/Schema/Country.pm
+++ lib/ClubSpain/Schema/Country.pm
@@ -46,7 +46,7 @@ __PACKAGE__->add_unique_constraint(on_alpha2  => [qw(alpha2)]);
 __PACKAGE__->add_unique_constraint(on_alpha3  => [qw(alpha3)]);
 __PACKAGE__->add_unique_constraint(on_numerics => [qw(numerics)]);
 
-__PACKAGE__->has_many(items => 'ClubSpain::Schema::City',
+__PACKAGE__->has_many(cities => 'ClubSpain::Schema::City',
     {'foreign.country_id' => 'self.id'}
 );
 
