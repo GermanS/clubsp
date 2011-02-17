8929bb1ebdfde6c91cdfc96efca3b175daf63c34
GermanS <german.semenkov@gmail.com>
2011-02-17 16:26:06 +0300

    add tests

--- lib/ClubSpain/Schema/Segment.pm
+++ lib/ClubSpain/Schema/Segment.pm
@@ -7,10 +7,14 @@ BEGIN {
     use base qw(DBIx::Class);
 };
 
-__PACKAGE__->load_components(qw(Core));
+__PACKAGE__->load_components(qw(Core PK::Auto));
 __PACKAGE__->table('segment');
 __PACKAGE__->source_name('Segment');
 __PACKAGE__->add_columns(
+    id => {
+        data_type         => 'integer',
+        is_auto_increment => 1
+    },
     timetable_id => {
         data_type      => 'integer',
         is_nullable    => 0,
@@ -28,11 +32,11 @@ __PACKAGE__->add_columns(
     },
  );
 
- __PACKAGE__->add_unique_constraint(on_timetable_class_price => [qw(timetable_id fare_id fare_class_id)]);
+__PACKAGE__->set_primary_key('id');
 
- __PACKAGE__->belongs_to('timetable'  => 'ClubSpain::Schema::TimeTable', 'timetable_id');
- __PACKAGE__->belongs_to('fare'       => 'ClubSpain::Schema::Fare',      'fare_id');
- __PACKAGE__->belongs_to('fare_class' => 'ClubSpain::Schema::FareClass', 'fare_class_id');
+__PACKAGE__->belongs_to('timetable'  => 'ClubSpain::Schema::TimeTable', 'timetable_id');
+__PACKAGE__->belongs_to('fare'       => 'ClubSpain::Schema::Fare',      'fare_id');
+__PACKAGE__->belongs_to('fare_class' => 'ClubSpain::Schema::FareClass', 'fare_class_id');
 
 
  1;
---
+++ t/design/segment/segment_delete.t
@@ -0,0 +1,31 @@
+use Test::More tests => 6;
+
+use strict;
+use warnings;
+
+use_ok('ClubSpain::Model::Segment');
+
+use lib qw(t/lib);
+use ClubSpain::Test;
+
+my $schema = ClubSpain::Test->init_schema();
+my $count = $schema->resultset('Segment')->search({})->count;
+
+my $segment = ClubSpain::Model::Segment->new(
+    fare_class_id => 1,
+    fare_id       => 3,
+    timetable_id  => 1,
+);
+
+my $object = $segment->create();
+
+isa_ok($object, 'ClubSpain::Schema::Segment');
+is($object->fare_class_id, 1, 'got fare class id');
+is($object->fare_id,       3, 'got fare id');
+is($object->timetable_id,  1, 'got timetable id');
+
+
+ClubSpain::Model::Segment->delete($object->id);
+
+my $rs = $schema->resultset('Segment')->search({});
+is($rs->count, $count, 'no objects left');
