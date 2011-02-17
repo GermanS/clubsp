ac54dacf08efc085cf340a88641e4f434e93e159
GermanS <german.semenkov@gmail.com>
2011-02-17 15:30:08 +0300

    add test

--- lib/ClubSpain/Model/Fare.pm
+++ lib/ClubSpain/Model/Fare.pm
@@ -9,9 +9,26 @@ use MooseX::ClassAttribute;
 class_has '+source_name' => ( default => sub  { 'Fare' });
 
 has 'id'            => ( is => 'ro' );
-has 'is_published'  => ( is => 'ro', required => 1 );
-has 'fare'         => ( is => 'ro', required => 1 );
+has 'fare'          => ( is => 'ro', required => 1 );
 
 #has 'segments'      => { is => 'ro', required => 1, isa => 'ArreyRef' };
+
+sub create {
+    my $self = shift;
+
+    $self->SUPER::create({
+        fare  => $self->fare,
+    });
+}
+
+sub update {
+    my $self = shift;
+
+    $self->check_for_class_method();
+
+    $self->SUPER::update({
+        fare  => $self->fare,
+    });
+}
 
 1;
--- t/design/fare/fare.t
+++ t/design/fare/fare.t
@@ -6,10 +6,8 @@ use warnings;
 use_ok('ClubSpain::Model::Fare');
 
 my $fare = ClubSpain::Model::Fare->new(
-    is_published  => 1,
     fare          => 1000,
 );
 
 isa_ok($fare, 'ClubSpain::Model::Fare');
-is($fare->is_published, 1, 'got is published');
 is($fare->fare, 1000, 'got price');
