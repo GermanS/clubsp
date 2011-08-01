ef756f59752723fb965796df6b6bffd63c10ab2c
GermanS <german.semenkov@gmail.com>
2011-08-01 23:47:41 +0400

    refactoring. add documentaton

--- lib/ClubSpain/Model/TimeTable.pm
+++ lib/ClubSpain/Model/TimeTable.pm
@@ -8,6 +8,50 @@ use ClubSpain::Types;
 use MooseX::ClassAttribute;
 class_has '+source_name' => ( default => sub  { 'TimeTable' });
 
+=head2 Поля
+
+=over
+
+=item id
+
+    Идентификатор объекта
+
+=item is_published
+
+    Флаг опубликованности
+
+=item flight_id
+
+    Идентификатор рейса
+
+=item departure_date
+
+    Дата отправления
+
+=item departure_time
+
+    Время отправления
+
+=item arrival_date
+
+    Дата прибытия
+
+=item arrival_time
+
+    Время прибытия
+
+=item departure_terminal_id
+
+    Идентификатор терминала отправления
+
+=item arrival_terminal_id
+
+    Идентификатор терминала прибытия
+
+=back
+
+=cut
+
 has 'id'                    => ( is => 'ro' );
 has 'is_published'          => ( is => 'ro', required => 1 );
 has 'flight_id'             => ( is => 'ro', required => 1 );
@@ -18,6 +62,12 @@ has 'arrival_time'          => ( is => 'ro', required => 1 );
 has 'airplane_id'           => ( is => 'ro' );
 has 'departure_terminal_id' => ( is => 'ro' );
 has 'arrival_terminal_id'   => ( is => 'ro' );
+
+=head2 create()
+
+    Добавление объекта в базу данных.
+
+=cut
 
 sub create {
     my $self = shift;
@@ -34,6 +84,12 @@ sub create {
         arrival_terminal_id     => $self->arrival_terminal_id,
     });
 }
+
+=head2 update()
+
+    Редактирование обхекта в базе данных
+
+=cut
 
 sub update {
     my $self = shift;
@@ -53,23 +109,22 @@ sub update {
     });
 }
 
-sub searchDatesOfDeparture {
-    my ($self, %params) = @_;
+=head2 departures(cityOfDeparture => , [duration => ])
 
-    return
-        $self->schema()
-             ->resultset($self->source_name)
-             ->searchDatesOfDeparture(%params);
-}
+    Получение расписание вылетов из указанного города начиная с текущего момента
+    плюс колическов указанных дней
 
-sub searchTimetable {
-    my ($self, %params) = @_;
+На входе:
 
-    return
-        $self->schema()
-             ->resultset($self->source_name)
-             ->searchTimetable(%params);
-}
+    cityOfDeparture - город вылета
+    duration        - количество дней за которое необходимо паказать расписание
+                      (необязательный параметр)
+
+На выходе
+
+    $iterator - итератор расписаний
+
+=cut
 
 sub departures {
     my ($self, %params) = @_;
@@ -80,6 +135,23 @@ sub departures {
              ->departures(%params);
 }
 
+=head2 arrivals(cityOfArrival => , [duration => ])
+
+    Получение расписания прилетов в указанный город начиная с текущего момента
+    плюс колическов указанных дней
+
+На входе:
+
+    cityOfArrival - город прилета
+    duration      - количество дней за которое необходимо паказать расписание
+                    (необязательный параметр)
+
+Нам выходе
+
+    $iterator  - итератор расписаний
+
+=cut
+
 sub arrivals {
     my ($self, %params) = @_;
 
@@ -87,6 +159,106 @@ sub arrivals {
         $self->schema()
              ->resultset($self->source_name)
              ->arrivals(%params);
+}
+
+=head2 searchCitiesOfDeparture()
+
+    Получение списка городов отправления из расписания
+
+На выходе:
+
+    $iterator - итератор городов отправления в расписании
+
+У всех объектов: стран, городов, аэропортов, ... должен стоять флаг is_published=1
+
+=cut
+
+sub searchCitiesOfDeparture {
+    my $self = shift;
+
+    return
+        $self->schema()
+             ->resultset('ViewTimeTable')
+             ->searchCitiesOfDeparture();
+}
+
+=head2 searchCitiesOfArrival(cityOfDeparture => )
+
+    Получение списка гододов прибытия из указанного города отправления
+
+На входе:
+
+    cityOfDeparture - годод отправления
+
+Но выходе:
+
+    $iterator - итератор городов прибытия из города отправления
+
+У всех объектов: стран, городов, аэропортов, ... должен стоять флаг is_published=1
+
+=cut
+
+sub searchCitiesOfArrival {
+    my ($self, %params) = @_;
+
+    return
+        $self->schema()
+             ->resultset('ViewTimeTable')
+             ->searchCitiesOfArrival(%params);
+}
+
+=head2 searchDatesOfDeparture(cityOfDeparture => , cityOfArrival =>, [ startDate => ] )
+
+    Получение дат отправления из города cityOfDeparture в город cityOfArrival
+    начиная с даты startDate
+
+На входе:
+
+    cityOfDeparture - годор отправления
+    cityOfArrival   - город прибытия
+    startDate       - дата начала (необязательный параметр)
+
+На выходе:
+
+  $iterator - итератор дат вылета
+
+У всех объектов: стран, городов, аэропортов, ... должен стоять флаг is_published=1
+
+=cut
+
+sub searchDatesOfDeparture {
+    my ($self, %params) = @_;
+
+    return
+        $self->schema()
+             ->resultset('ViewTimeTable')
+             ->searchDatesOfDeparture(%params);
+}
+
+=head2 searchTimetable(cityOfDeparture => , cityOfArrival => , dateOfDeparture =>)
+
+    Получение расписания по критерию
+
+На входе:
+
+    cityOfDeparture - город отправление
+    cityOfArrival   - город прибытия
+    dateOfDeparture - дата отправления
+
+На выходе:
+
+    $iterator всех найденных вариантов удовлетворяющих критериям
+    или undef если не заданы обязательные параметры.
+
+=cut
+
+sub searchTimetable {
+    my ($self, %params) = @_;
+
+    return
+        $self->schema()
+             ->resultset('ViewTimeTable')
+             ->searchTimetable(%params);
 }
 
 __PACKAGE__->meta->make_immutable;
