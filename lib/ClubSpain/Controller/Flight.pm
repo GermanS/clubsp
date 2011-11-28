b36e2d5a9e5e926139b97678bffa9df10067d280
GermanS <german.semenkov@gmail.com>
2011-11-28 16:54:04 +0300

    impl search()

--- lib/ClubSpain/Controller/Flight.pm
+++ lib/ClubSpain/Controller/Flight.pm
@@ -10,6 +10,7 @@ use ClubSpain::XML::VipService::Flight;
 use ClubSpain::XML::VipService;
 use ClubSpain::XML::VipService::Config;
 use DateTime;
+use DateTime::Format::Strptime;
 
 sub auto :Private {
     my ($self, $c) = @_;
@@ -23,6 +24,9 @@ sub base :Chained('/flight') :PathPart('') :CaptureArgs(0) {
 }
 
 sub end :ActionClass('RenderView') {
+    my ($self, $c) = @_;
+
+    $self->setup_stash_from_request($c);
 }
 
 sub default :Path {
@@ -31,46 +35,77 @@ sub default :Path {
 sub search :Local {
     my ($self, $c) = @_;
 
-    my $seat = ClubSpain::XML::VipService::Seat->new(
-        passenger => 'ADULT',
-        count     => 1
-    );
+    my $flight;
 
-    my $cityOfDeparture1 = ClubSpain::XML::VipService::Location->new(
-        code => 'MOW',
-        name => 'Moscow'
-    );
-    my $cityOfArrival1 = ClubSpain::XML::VipService::Location->new(
-        code => 'BCN',
-        name => 'barcelona'
-    );
-    my $cityOfDeparture2 = ClubSpain::XML::VipService::Location->new(
-        code => 'BCN',
-        name => 'Barcelona'
-    );
-    my $cityOfArrival2 = ClubSpain::XML::VipService::Location->new(
-        code => 'MOW',
-        name => 'Moscow'
-    );
+    eval {
+        my $city_of_departure = $c->model('City')->fetch_by_id(
+            $c->request->param('CityOfDeparture_id')
+        );
+        my $city_of_arrival = $c->model('City')->fetch_by_id(
+            $c->request->param('CityOfArrival_id')
+        );
+        my $cityOfDeparture = ClubSpain::XML::VipService::Location->new(
+            code => $city_of_departure->iata,
+            name => $city_of_departure->name,
+        );
+        my $cityOfArrival = ClubSpain::XML::VipService::Location->new(
+            code => $city_of_arrival->iata,
+            name => $city_of_arrival->name,
+        );
 
-    my $date1 = DateTime->new( year => 2012, month => 01, day => 21 );
-    my $date2 = DateTime->new( year => 2012, month => 01, day => 28 );
+        my $format = DateTime::Format::Strptime->new(
+            locale  => 'ru_RU',
+            pattern => '%d-%m-%Y',
+        );
+        my $dateOfDeparture1 = $format->parse_datetime(
+            $c->request->param('DateOfDeparture1')
+        );
+        my $dateOfDeparture2;
+        if ($c->request->param('DateOfDeparture2')) {
+            $dateOfDeparture2 = $format->parse_datetime(
+                $c->request->param('DateOfDeparture2')
+            );
+        }
 
-    my $route1 = ClubSpain::XML::VipService::Route->new(
-        date          => $date1, #'2012-01-21',
-        locationBegin => $cityOfDeparture1,
-        locationEnd   => $cityOfArrival1,
-    );
-    my $route2 = ClubSpain::XML::VipService::Route->new(
-        date          => $date2, #'2012-01-28',
-        locationBegin => $cityOfDeparture2,
-        locationEnd   => $cityOfArrival2,
-    );
+        my @seat = ();
+        my %values = (
+            'ADULT'  => 'adults_data',
+            'CHILD'  => 'children_data',
+            'INFANT' => 'infants_data',
+        );
+        while ( my ($key, $value) = each %values) {
+            my $count = $c->request->param($value);
+            push @seat, ClubSpain::XML::VipService::Seat->new(
+                passenger => $key,
+                count     => $count,
+            ) if $count;
+        }
 
-    my $flight = ClubSpain::XML::VipService::Flight->new(
-        route => [ $route1, $route2 ],
-        seat  => [ $seat ]
-    );
+        my @route;
+        push @route, ClubSpain::XML::VipService::Route->new(
+            date          => $dateOfDeparture1,
+            locationBegin => $cityOfDeparture,
+            locationEnd   => $cityOfArrival,
+        );
+        push @route, ClubSpain::XML::VipService::Route->new(
+            date          => $dateOfDeparture2,
+            locationBegin => $cityOfArrival,
+            locationEnd   => $cityOfDeparture,
+        ) if $dateOfDeparture2;
+
+        $flight = ClubSpain::XML::VipService::Flight->new(
+            serviceClass => $c->request->param('cabin_data'),
+            route => \@route,
+            seat  => \@seat
+        );
+    };
+
+    if ($@) {
+        $c->stash(
+            message => ClubSpain::Message->error( 'Произошла ошибка. Попробуйте еще раз!' )
+        );
+        return;
+    }
 
     my $config = ClubSpain::XML::VipService::Config->new(
         locale              => 'ru', #TODO: get rid of this
@@ -85,6 +120,55 @@ sub search :Local {
     my $result = $service->searchFlights($flight);
 
     $c->stash( result => $result );
+}
+
+sub setup_stash_from_request : Private {
+    my ($self, $c) = @_;
+
+    my %result = ();
+
+    my @values = qw(
+        adults_data
+        children_data
+        infants_data
+        cabin_data
+        CityOfArrival
+        CityOfArrival_id
+        CityOfDeparture
+        CityOfDeparture_id
+        DateOfDeparture1
+        DateOfDeparture2
+        search_mode
+    );
+    for my $value (@values) {
+        $result{$value} = $c->request->param($value) || 0;
+    }
+
+    #set adults default to 1
+    $result{'adults_data'} = 1
+        unless $result{'adults_data'};
+
+    #set cabin default to 'ECONOMY'
+    $result{'cabin_data'} = 'ECONOMY'
+        unless $result{'cabin_data'};
+
+    #set dates of departure to undef
+    for my $date (qw(DateOfDeparture1 DateOfDeparture2)) {
+        $result{$date} = ''
+            unless $result{$date};
+    }
+
+    #set cities defaults to undef
+    for my $city qw(CityOfArrival CityOfDeparture) {
+        $result{$city} = ''
+            unless $result{$city}
+    }
+
+    #set search mode to RT
+    $result{'search_mode'} = 'RT'
+        unless $result{'search_mode'};
+
+    $c->stash({%result});
 }
 
 1;
