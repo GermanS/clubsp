c6307beeb597f2bca8a0006ff1d3d52e71c263a1
GermanS <german.semenkov@gmail.com>
2011-11-28 16:39:58 +0300

    add price

---
+++ lib/ClubSpain/XML/VipService/Response/FlightSearch/Price/Adult.pm
@@ -0,0 +1,12 @@
+package ClubSpain::XML::VipService::Response::FlightSearch::Price::Adult;
+use strict;
+use warnings;
+use utf8;
+use Moose;
+
+has 'amount'      => ( is => 'rw' );
+has 'elementType' => ( is => 'ro' );
+
+__PACKAGE__->meta->make_immutable();
+
+1;
