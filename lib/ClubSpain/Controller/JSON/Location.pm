55465bf0e217a03b728b6c8f6071061bc688c951
GermanS <german.semenkov@gmail.com>
2012-02-28 16:18:56 +0400

    add search location related methods

--- lib/ClubSpain/Controller/JSON/Location.pm
+++ lib/ClubSpain/Controller/JSON/Location.pm
@@ -1,8 +1,8 @@
-package ClubSpain::Controller::JSON::City;
+package ClubSpain::Controller::JSON::Location;
 use strict;
 use warnings;
 use utf8;
-use base qw(ClubSpain::Controller::RPC);
+use base qw(ClubSpain::Controller::RPC::Flight);
 
 
 =head2 suggest()
@@ -21,6 +21,82 @@ sub suggest : Local {
     my @res = $self->_suggest($c, $search);
     $c->stash(json_data => \@res);
 
+    $c->forward('View::JSON');
+}
+
+
+
+#список стран отправлениия
+sub getCountryOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getCountryOfDeparture($c);
+
+    $c->stash(json_data => \@res);
+    $c->forward('View::JSON');
+}
+
+
+
+#список городов отправления
+sub getCityOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getCityOfDeparture(
+        $c, $c->request->param('countryOfDeparture')
+    );
+
+    $c->stash(json_data => \@res);
+    $c->forward('View::JSON');
+}
+
+
+
+#список аэропортов отправления
+sub getAirportOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getAirportOfDeparture(
+        $c, $c->request->param('cityOfDeparture')
+    );
+
+    $c->stash(json_data => \@res);
+    $c->forward('View::JSON');
+}
+
+
+
+#список стран прибытия
+sub getCountryOfArrival :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getCountryOfArrival($c);;
+
+    $c->stash(json_data => \@res);
+    $c->forward('View::JSON');
+}
+
+
+
+#список городов прибытия
+sub getCityOfArrival :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getCityOfArrival($c, $c->request->param('countryOfArrival'));
+
+    $c->stash(json_data => \@res);
+    $c->forward('View::JSON');
+}
+
+
+
+#список аэропортов прибытия
+sub getAirportOfArrival :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->_getAirportOfArrival($c, $c->request->param('cityOfArrival'));
+
+    $c->stash(json_data => \@res);
     $c->forward('View::JSON');
 }
 
