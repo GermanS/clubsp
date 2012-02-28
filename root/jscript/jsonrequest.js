645011e355114f0f00bd6e847a6eb3d1398a337f
GermanS <german.semenkov@gmail.com>
2012-02-28 13:19:19 +0400

    jquery json requests

---
+++ root/jscript/jsonrequest.js
@@ -0,0 +1,50 @@
+var FareSearchCitiesOfDepartureOW  = '/json/charter/searchCitiesOfDepartureOW';
+var FareSearchCitiesOfArrivalOW    = '/json/charter/searchCitiesOfArrivalOW';
+var FareSearchDatesOfDepartureOW   = '/json/charter/searchDatesOfDepartureOW';
+var FareSearchCitiesOfDeparture1RT = '/json/charter/searchCitiesOfDeparture1RT';
+var FareSearchCitiesOfArrival1RT   = '/json/charter/searchCitiesOfArrival1RT';
+var FareSearchCitiesOfDeparture2RT = '/json/charter/searchCitiesOfDeparture2RT';
+var FareSearchCitiesOfArrival2RT   = '/json/charter/searchCitiesOfArrival2RT';
+var FareSearchDatesOfDeparture1RT  = '/json/charter/searchDatesOfDeparture1RT';
+var FareSearchDatesOfDeparture2RT  = '/json/charter/searchDatesOfDeparture2RT';
+
+/* common functions */
+var FareSearchCitiesOfDeparture = '/json/charter/searchCitiesOfDeparture';
+var FareSearchCitiesOfArrival   = '/json/charter/searchCitiesOfArrival';
+var FareSearchDatesOfDeparture  = '/json/charter/searchDatesOfDeparture';
+var FareSearchFlights           = '/json/charter/searchFlights';
+var FareSearchTimetable         = '/json/charter/searchTimetable';
+
+/* Timetable requests */
+var Timetable_getCitiesOfDeparture  = '/json/charter/timetable/getCitiesOfDeparture';
+var Timetable_getCitiesOfArrival    = '/json/charter/timetable/getCitiesOfArrival';
+var Timetable_searchFlights         = '/json/charter/timetable/searchFlights';
+
+
+function makeRequest(method, params, element, initialValue) {
+    var request = $.ajax({
+        url: method,
+        dataType: 'json',
+        data: params,
+        success : function(result) {
+            if (element && element[0]) {
+                element[0].options.length = 0;
+
+                $(result).each(function() {
+                    var option = document.createElement("option");
+                    option.text = $(this).attr('name');
+                    option.value = $(this).attr('id');
+
+                    element.append(option);
+                });
+
+                element.val(initialValue);
+                element.attr('disabled', false);
+                element.change();
+            }
+        }
+    });
+
+}
+
+
