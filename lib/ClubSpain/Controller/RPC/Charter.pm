981c2d17510bbc5ce9e794fedf5cdb4b6628d21b
GermanS <german.semenkov@gmail.com>
2012-05-28 20:21:40 +0400

    refactoring

--- lib/ClubSpain/Controller/RPC/Charter.pm
+++ lib/ClubSpain/Controller/RPC/Charter.pm
@@ -2,24 +2,24 @@ package ClubSpain::Controller::RPC::Charter;
 use strict;
 use warnings;
 use utf8;
-use base qw(Catalyst::Controller);
+use base qw(ClubSpain::Controller::RPC);
 
 sub searchCitiesOfDeparture {
     my $self = shift;
 
     my $iterator = $self->_app->model('TimeTable')->searchCitiesOfDeparture();
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 sub searchCitiesOfArrival {
     my ($self, %params) = @_;
 
     my $iterator = $self->_app->model('TimeTable')->searchCitiesOfArrival(
-      cityOfDeparture => $params{'cityOfDeparture'}
+        cityOfDeparture => $params{'cityOfDeparture'}
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 sub searchDatesOfDeparture {
@@ -31,12 +31,7 @@ sub searchDatesOfDeparture {
         startDate       => $params{'startDate'},
     );
 
-    my @res;
-    while (my $item = $iterator->next) {
-        push @res, { id => $item->departure_date, name => $item->departure_date };
-    }
-
-    return @res;
+    return $self->object_to_date_hash($iterator);
 }
 
 sub searchFlights {
@@ -64,12 +59,14 @@ sub searchFlights {
 =head2 searchTimetable(%params)
 
     Выбор расписания по критерию
-    входные параметры
-        cityOfDeparture - идентификатор города отправления
-        cityOfArrival   - идентификатор города прибытия
-        dateOfDeparture - дата отправления
 
-    возвращается хеш с идентификаторами расписания и названиеми рейсов
+Входные параметры
+
+    cityOfDeparture - идентификатор города отправления
+    cityOfArrival   - идентификатор города прибытия
+    dateOfDeparture - дата отправления
+
+    возвращается хеш с идентификаторами расписания и названиями рейсов
 
 =cut
 
@@ -82,7 +79,7 @@ sub searchTimetable {
         dateOfDeparture => $params{'dateOfDeparture'},
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 
@@ -97,7 +94,7 @@ sub searchCitiesOfDepartureOW {
 
     my $iterator = $self->_app->model('City')->searchCitiesOfDepartureOW();
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchCitiesOfArrivalOW(cityOfDeparture => );
@@ -114,7 +111,7 @@ sub searchCitiesOfArrivalOW {
         cityOfDeparture => $params{'cityOfDeparture'}
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchDatesOfDepartureOW(cityOfDeparture => , cityOfArrival => )
@@ -132,7 +129,7 @@ sub searchDatesOfDepartureOW {
         cityOfArrival   => $params{'cityOfArrival'},
     );
 
-    return date_hash($iterator);
+    return $self->to_date_hash($iterator);
 }
 
 =head2 searchCitiesOfDeparture1RT()
@@ -145,15 +142,17 @@ sub searchCitiesOfDeparture1RT {
     my $self = shift;
 
     my $iterator = $self->_app->model('City')->searchCitiesOfDeparture1RT();
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchCitiesOfArrival1RT(cityOfDeparture1 => )
 
     Поиск городов прибытия при отправлении из города cityOfDeparture1
     на первом сегменте тарифов RT
-    На входе:
-        cityOfDeparture1 - идентификатор города отправления 1го сегмента
+
+На входе:
+
+    cityOfDeparture1 - идентификатор города отправления 1го сегмента
 
 =cut
 
@@ -164,15 +163,17 @@ sub searchCitiesOfArrival1RT {
         cityOfDeparture1 => $params{'cityOfDeparture1'}
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchCitiesOfDeparture2RT(cityOfDeparture1 => , cityOfArrival1 => )
 
     Поиск городов отправления в тарифах RT на втором сегменте
-    На входе:
-        cityOfDeparture1 - идентификатор города отправления 1го сегмента
-        cityOfArrival1   - идентификатор города прибытия 1го сегмента
+
+На входе:
+
+    cityOfDeparture1 - идентификатор города отправления 1го сегмента
+    cityOfArrival1   - идентификатор города прибытия 1го сегмента
 
 =cut
 
@@ -184,16 +185,18 @@ sub searchCitiesOfDeparture2RT {
         cityOfArrival1   => $params{'cityOfArrival1'},
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchCitiesOfArrival2RT(cityOfDeparture1 => , cityOfArrival1 =>, cityOfDeparture2 => )
 
     Поиск городов прибытия на втором сегменте в тарифах RT
-    На входе:
-        cityOfDeparture1 - идентификатор города отправления 1го сегмента
-        cityOfArrival1   - идентификатор города прибытия 1го сегмента
-        cityOfDeparture2 - идентификатор города отправления 2го сегмента
+
+На входе:
+
+    cityOfDeparture1 - идентификатор города отправления 1го сегмента
+    cityOfArrival1   - идентификатор города прибытия 1го сегмента
+    cityOfDeparture2 - идентификатор города отправления 2го сегмента
 
 =cut
 
@@ -206,17 +209,19 @@ sub searchCitiesOfArrival2RT {
         cityOfDeparture2 => $params{'cityOfDeparture2'}
     );
 
-    return id_name_hash($iterator);
+    return $self->to_array($iterator);
 }
 
 =head2 searchDatesOfDeparture1RT(cityOfDeparture1 => , cityOfArrival1, cityOfDeparture2 => , cityOfArrival2 => )
 
     Поиск дат выполнения рейса на первом сегменте
-    На входе:
-        cityOfDeparture1 - идентификатор города отправления 1го сегмента
-        cityOfArrival1   - идентификатор города прибытия 1го сегмента
-        cityOfDeparture2 - идентификатор города отправления 2го сегмента
-        cityOfArrival2   - идентификатор города прибытия 2го сегмента
+
+На входе:
+
+    cityOfDeparture1 - идентификатор города отправления 1го сегмента
+    cityOfArrival1   - идентификатор города прибытия 1го сегмента
+    cityOfDeparture2 - идентификатор города отправления 2го сегмента
+    cityOfArrival2   - идентификатор города прибытия 2го сегмента
 
 =cut
 
@@ -230,18 +235,20 @@ sub searchDatesOfDeparture1RT {
         cityOfArrival2   => $params{'cityOfArrival2'},
     );
 
-    return date_hash($iterator);
+    return $self->to_date_hash($iterator);
 }
 
 =head2 searchDatesOfDeparture2RT(cityOfDeparture1 => , cityOfArrival1 => , cityOfDeparture2 =>, cityOfArrival2 =>, dateOfDeparture1 =>)
 
     Поиск дат выполнения рейса на втором сегменте
-    на входе:
-        cityOfDeparture1 - идентификатор города отправления 1го сегмента
-        cityOfArrival1   - идентификатор города прибытия 1го сегмента
-        cityOfDeparture2 - идентификатор города отправления 2го сегмента
-        cityOfArrival2   - идентификатор города прибытия 2го сегмента
-        dateOfDeparture1 - дата первого сегмента
+
+На входе:
+
+    cityOfDeparture1 - идентификатор города отправления 1го сегмента
+    cityOfArrival1   - идентификатор города прибытия 1го сегмента
+    cityOfDeparture2 - идентификатор города отправления 2го сегмента
+    cityOfArrival2   - идентификатор города прибытия 2го сегмента
+    dateOfDeparture1 - дата первого сегмента
 
 =cut
 
@@ -256,16 +263,18 @@ sub searchDatesOfDeparture2RT {
         dateOfDeparture1 => $params{'dateOfDeparture1'},
     );
 
-    return date_hash($iterator);
+    return $self->to_date_hash($iterator);
 }
 
 =head2 searchRT({cityOfDeparture => , cityOfArrival =>, dateOfDeparture => }, {...})
 
     Поиск тарифов в обе стороны по заданным критериям.
-    На входе двухэлелементный массив хешей
-        cityOfDeparture - город отправления
-        cityOfArrival   - город прибытия
-        dateOfDeparture - дата рейса
+
+На входе: двухэлелементный массив хешей
+
+    cityOfDeparture - город отправления
+    cityOfArrival   - город прибытия
+    dateOfDeparture - дата рейса
 
 =cut
 
@@ -282,16 +291,18 @@ sub searchRT {
         dateOfDeparture => $params[1]{'dateOfDeparture'}
     });
 
-    return itinerary_2_hash($iterator);
+    return $self->itinerary_2_hash($iterator);
 }
 
 =head searchOW(cityOfDeparture => , cityOfArrival => , dateOfDeparture => )
 
     Поиск тарифа в одну сторону в тарифах OW
-    На входе
-        cityOfDeparture - идентификатор города отправления
-        cityOdArrival   - идентификатор города прибытия
-        dateOfDeparture - дата отправления
+
+На входе
+
+    cityOfDeparture - идентификатор города отправления
+    cityOdArrival   - идентификатор города прибытия
+    dateOfDeparture - дата отправления
 
 =cut
 
@@ -304,15 +315,17 @@ sub searchOW {
         dateOfDeparture => $params{'dateOfDeparture'}
     });
 
-    return itinerary_2_hash($iterator);
+    return $self->itinerary_2_hash($iterator);
 }
 
 =head2 viewRT({cityOfDeparture => , cityOdArrival => }, {...})
 
     Поиск всех тарифов RT по заданным городам
-    На входе 2х элементный массив хешей
-        cityOfDeparture - идентификатор города отправления
-        cityOfArrival   - идентификатор города прибытия
+
+На входе 2х элементный массив хешей
+
+    cityOfDeparture - идентификатор города отправления
+    cityOfArrival   - идентификатор города прибытия
 
 =cut
 
@@ -327,15 +340,17 @@ sub viewRT {
         cityOfArrival   => $params[1]{'cityOfArrival'}
     });
 
-    return itinerary_2_hash($iterator);
+    return $self->itinerary_2_hash($iterator);
 }
 
 =head2 viewOW(cityOfDepatrure => , cityOfArrival =>)
 
     Поиск всех тарифов в одну сторону из города cityOfDepatrure в город cityOfArrival
-    На входе
-        cityOfDeparture - идентификатор города отправления
-        cityOfArrival   - идентификатор города прибытия
+
+На входе
+
+    cityOfDeparture - идентификатор города отправления
+    cityOfArrival   - идентификатор города прибытия
 
 =cut
 
@@ -347,110 +362,9 @@ sub viewOW {
         cityOfArrival   => $params{'cityOfArrival'}
     });
 
-    return itinerary_2_hash($iterator);
+    return $self->itinerary_2_hash($iterator);
 }
 
-sub id_name_hash {
-    my $iterator = shift;
 
-    my @res;
-    while (my $row = $iterator->next) {
-        push @res, { id => $row->id, name => $row->name };
-    }
-
-    return @res;
-}
-
-sub date_hash {
-    my $iterator = shift;
-
-    my @res;
-    while (my $date = $iterator->next) {
-        push @res, {
-            id   => $date->get_column('dateOfDeparture'),
-            name => $date->get_column('dateOfDeparture')
-        };
-    }
-
-    return @res;
-}
-
-sub itinerary_2_hash {
-    my $iterator = shift;
-
-    my @res;
-    while (my $ticket = $iterator->next) {
-        push @res, {
-            id     => $ticket->{'id'},
-            price  => $ticket->{'cost'},
-            segments => segments($ticket),
-        }
-    }
-
-    return @res;
-}
-
-sub segments {
-    my $segment = shift;
-
-    my @res;
-    while ($segment) {
-        my $flightNumber = sprintf "%2s %4s", $segment->{'timetable'}->{'flight'}->{'airline'}->{'iata'},
-                                              $segment->{'timetable'}->{'flight'}->{'code'};
-
-        push @res, {
-            id            => $segment->{'timetable'}->{'id'},
-            isFree        => $segment->{'timetable'}->{'is_free'},
-            dateBegin     => $segment->{'timetable'}->{'departure_date'},
-            timeBegin     => $segment->{'timetable'}->{'departure_time'},
-            dateEnd       => $segment->{'timetable'}->{'arrival_date'},
-            timeEnd       => $segment->{'timetable'}->{'arrival_time'},
-            flightNumber  => $flightNumber,
-            board         => {
-                id   => $segment->{'timetable'}->{'airplane'}->{'id'},
-                code => $segment->{'timetable'}->{'airplane'}->{'iata'},
-                name => $segment->{'timetable'}->{'airplane'}->{'name'},
-            },
-            airline => {
-                id   => $segment->{'timetable'}->{'flight'}->{'airline'}->{'id'},
-                iata => $segment->{'timetable'}->{'flight'}->{'airline'}->{'iata'},
-                name => $segment->{'timetable'}->{'flight'}->{'airline'}->{'name'},
-            },
-            serviceClass => {
-                id   => $segment->{'fare_class'}->{'id'},
-                code => $segment->{'fare_class'}->{'code'},
-                name => $segment->{'fare_class'}->{'name'},
-            },
-            locationBegin => {
-                city => {
-                    id   => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'city'}->{'id'},
-                    iata => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'city'}->{'iata'},
-                    name => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'city'}->{'name'},
-                },
-                airport => {
-                    id   => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'id'},
-                    iata => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'iata'},
-                    name => $segment->{'timetable'}->{'flight'}->{'departure_airport'}->{'name'},
-                }
-            },
-            locationEnd => {
-                city => {
-                    id   => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'city'}->{'id'},
-                    iata => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'city'}->{'iata'},
-                    name => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'city'}->{'name'},
-                },
-                airport => {
-                    id   => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'id'},
-                    iata => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'iata'},
-                    name => $segment->{'timetable'}->{'flight'}->{'destination_airport'}->{'name'},
-                },
-            }
-        };
-
-        $segment = $segment->{'children'}[0];
-    }
-
-    return \@res;
-}
 
 1;
