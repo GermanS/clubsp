d420b8ccb815b0a83a4d36d779e02ad413c0bc72
GermanS <german.semenkov@gmail.com>
2012-05-28 20:22:12 +0400

    impl find*() methods for integrating with external sources

--- lib/ClubSpain/Controller/JSON/Charter/Timetable.pm
+++ lib/ClubSpain/Controller/JSON/Charter/Timetable.pm
@@ -13,7 +13,6 @@ sub getCitiesOfDeparture :Local {
 }
 
 
-
 #поиск в расписании городов прибытия
 sub getCitiesOfArrival :Local {
     my ($self, $c) = @_;
@@ -25,8 +24,6 @@ sub getCitiesOfArrival :Local {
     $c->stash(json_data => \@res);
 }
 
-
-
 #поиск  рейсов в расписании
 sub searchFlights :Local {
     my ($self, $c) = @_;
@@ -36,6 +33,47 @@ sub searchFlights :Local {
         cityOfArrival   => $c->request->param('cityOfArrival')
     });
 
+    $c->stash(json_data => \@res);
+}
+
+#передача городов отправления с внешним источникам
+sub findCitiesOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->next::method();
+    $c->stash(json_data => \@res);
+}
+
+#передача городов прибытия с внешним источникам
+sub findCitiesOfArrival :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $c->request->param('cityOfDeparture')
+    );
+    $c->stash(json_data => \@res);
+}
+
+#передача дат внешним источникам
+sub findDatesOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $c->request->param('cityOfDeparture'),
+        cityOfArrival   => $c->request->param('cityOfArrival')
+    );
+    $c->stash(json_data => \@res);
+}
+
+#передача расписания внешним источникам
+sub findFlights :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $c->request->param('cityOfDeparture'),
+        cityOfArrival   => $c->request->param('cityOfArrival'),
+        dateOfDeparture => $c->request->param('dateOfDeparture')
+    );
     $c->stash(json_data => \@res);
 }
 
