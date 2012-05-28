a5b0f6ba8c0e554faf5d051dcabb722ecc347f11
GermanS <german.semenkov@gmail.com>
2012-05-28 20:20:33 +0400

    impl find*() methods for integrating with external sources

--- lib/ClubSpain/Controller/XMLRPC/Charter/TimeTable.pm
+++ lib/ClubSpain/Controller/XMLRPC/Charter/TimeTable.pm
@@ -25,4 +25,45 @@ sub searchFlights : XMLRPC {
     $c->stash(xmlrpc => \@res);
 }
 
+#передача городов отправления с внешним источникам
+sub findCitiesOfDeparture :Local {
+    my ($self, $c) = @_;
+
+    my @res = $self->next::method();
+    $c->stash(json_data => \@res);
+}
+
+#передача городов прибытия с внешним источникам
+sub findCitiesOfArrival :Local {
+    my ($self, $c, %params) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $params{'cityOfDeparture'}
+    );
+    $c->stash(json_data => \@res);
+}
+
+#передача дат внешним источникам
+sub findDatesOfDeparture :Local {
+    my ($self, $c, %params) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $params{'cityOfDeparture'},
+        cityOfArrival   => $params{'cityOfArrival'}
+    );
+    $c->stash(json_data => \@res);
+}
+
+#передача расписания внешним источникам
+sub findFlights :Local {
+    my ($self, $c, %params) = @_;
+
+    my @res = $self->next::method(
+        cityOfDeparture => $params{'cityOfDeparture'},
+        cityOfArrival   => $params{'cityOfArrival'},
+        dateOfDeparture => $params{'dateOfDeparture'}
+    );
+    $c->stash(json_data => \@res);
+}
+
 1;
