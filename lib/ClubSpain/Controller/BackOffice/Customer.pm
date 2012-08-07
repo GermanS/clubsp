7bc8ce6266e18ca929ff3a351f948ece7184c4fa
GermanS <german.semenkov@gmail.com>
2012-08-07 17:34:07 +0400

    get rid of FormFu

--- lib/ClubSpain/Controller/BackOffice/Customer.pm
+++ lib/ClubSpain/Controller/BackOffice/Customer.pm
@@ -1,9 +1,93 @@
 package ClubSpain::Controller::BackOffice::Customer;
-use strict;
-use warnings;
-use utf8;
-use parent qw(ClubSpain::Controller::BackOffice::FormFu);
-use ClubSpain::Constants qw(:all);
+use Moose;
+use namespace::autoclean;
+BEGIN {
+    extends 'Catalyst::Controller'
+};
+    with 'ClubSpain::Controller::BackOffice::BaseRole';
+
+use ClubSpain::Form::BackOffice::Customer;
+sub form :Private {
+    my ($self, $model) = @_;
+    return ClubSpain::Form::BackOffice::Customer->new( model_object => $model );
+};
+
+has 'template' => (
+    is => 'ro',
+    default => 'backoffice/customer/customer.tt2'
+);
+
+has 'template_form' => (
+    is => 'ro',
+    default => 'backoffice/customer/customer_form.tt2'
+);
+
+has 'model' => (
+    is => 'ro',
+    default => 'Customer',
+);
+
+sub default :Path {
+    my ($self, $c) = @_;
+    $c->stash( iterator => $c->model($self->model)->search({}) );
+};
+
+sub base :Chained('/backoffice/base') :PathPart('customer') :CaptureArgs(0) {};
+
+sub create :Local {
+    my ($self, $c) = @_;
+
+    my $customer = $c->model($self->model)->new();
+    my $form = $self->form($customer);
+    $form->process($c->request->parameters);
+
+    if ($form->validated) {
+        $customer->set_enable();
+
+        eval { $customer->create(); };
+        $form->process_error($@) if $@;
+    }
+
+    $c->stash(
+        form     => $form,
+        template => $self->template_form,
+    );
+};
+
+sub edit :Chained('id') :PathPart('edit') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $customer = $c->model($self->model)->new();
+    my $form = $self->form($customer);
+    $form->process(
+        init_object => {
+            name    => $self->get_object($c)->name,
+            surname => $self->get_object($c)->surname,
+            email   => $self->get_object($c)->email,
+            passwd  => $self->get_object($c)->passwd,
+            mobile  => $self->get_object($c)->mobile,
+        },
+        params => $c->request->parameters
+    );
+
+    if ($form->validated) {
+        eval {
+            $customer->id( $self->get_object($c)->id );
+            $customer->is_published( $self->get_object($c)->is_published );
+            $customer->update();
+        };
+
+        $form->process_error($@) if $@;
+    }
+
+    $c->stash(
+        form     => $form,
+        template => $self->template_form
+    );
+};
+
+
+=head
 
 sub auto :Private {
     my ($self, $c) = @_;
@@ -178,5 +262,9 @@ sub update :Private {
     $self->process_error($c, $@)
          if $@;
 };
+
+=cut
+
+__PACKAGE__->meta->make_immutable();
 
 1;
