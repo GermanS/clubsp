fb6f0f6ee5625949b3b666fe8d825526a94bcb52
GermanS <german.semenkov@gmail.com>
2012-09-10 14:58:03 +0400

    refactoring

--- lib/ClubSpain/Model/Company.pm
+++ lib/ClubSpain/Model/Company.pm
@@ -1,70 +1,105 @@
 package ClubSpain::Model::Company;
 use Moose;
-use utf8;
 use namespace::autoclean;
+use utf8;
 use parent qw(ClubSpain::Model::Base);
 use ClubSpain::Types;
 
 use MooseX::ClassAttribute;
 class_has '+source_name' => ( default => sub  { 'Company' });
 
-has 'id'            => ( is => 'rw' );
-has 'zipcode'       => ( is => 'rw' );
-has 'street'        => ( is => 'rw' );
-has 'company'       => ( is => 'rw' );
-has 'nick'          => ( is => 'rw' );
-has 'website'       => ( is => 'rw' );
-has 'INN'           => ( is => 'rw' );
-has 'OKPO'          => ( is => 'rw' );
-has 'OKVED'         => ( is => 'rw' );
-has 'is_NDS'        => ( is => 'rw' );
-has 'is_published'  => ( is => 'rw' );
+has 'id' => (
+    is      => 'rw',
+    reader  => 'get_id',
+    writer  => 'set_id',
+);
+has 'zipcode' => (
+    is      => 'rw',
+    reader  => 'get_zipcode',
+    writer  => 'set_zipcode',
+);
+has 'street'=> (
+    is      => 'rw',
+    reader  => 'get_street',
+    writer  => 'set_street',
+);
+has 'name' => (
+    is      => 'rw',
+    reader  => 'get_name',
+    writer  => 'set_name',
+);
+has 'nick' => (
+    is      => 'rw',
+    reader  => 'get_nick',
+    writer  => 'set_nick',
+);
+has 'website' => (
+    is      => 'rw',
+    reader  => 'get_website',
+    writer  => 'set_website',
+);
+has 'INN' => (
+    is      => 'rw',
+    reader  => 'get_INN',
+    writer  => 'set_INN',
+);
+has 'OKPO' => (
+    is      => 'rw',
+    reader  => 'get_OKPO',
+    writer  => 'set_OKPO',
+);
+has 'OKVED' => (
+    is      => 'rw',
+    reader  => 'get_OKVED',
+    writer  => 'set_OKVED',
+);
+has 'is_NDS' => (
+    is      => 'rw',
+    reader  => 'get_is_NDS',
+    writer  => 'set_is_NDS',
+);
+has 'is_published' => (
+    is      => 'rw',
+    reader  => 'get_is_published',
+    writer  => 'set_is_published',
+);
 
 with 'ClubSpain::Model::Role::Company';
 
 sub validate_zipcode {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('zipcode')->type_constraint->validate($value);
 }
 sub validate_street  {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('street')->type_constraint->validate($value);
 }
-sub validate_company {
-    my $self = shift;
-    my $value = shift;
-    $self->meta()->get_attribute('company')->type_constraint->validate($value);
+sub validate_name {
+    my ($self, $value) = @_;
+    $self->meta()->get_attribute('name')->type_constraint->validate($value);
 }
 sub validate_nick {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('nick')->type_constraint->validate($value);
 }
 sub validate_website {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('website')->type_constraint->validate($value);
 }
 sub validate_INN  {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('INN')->type_constraint->validate($value);
 }
 sub validate_OKPO {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('OKPO')->type_constraint->validate($value);
 }
 sub validate_OKVED {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('OKVED')->type_constraint->validate($value);
 }
 sub validate_is_NDS  {
-    my $self = shift;
-    my $value = shift;
+    my ($self, $value) = @_;
     $self->meta()->get_attribute('is_NDS')->type_constraint->validate($value);
 }
 
@@ -85,17 +120,16 @@ sub params {
     my $self = shift;
 
     return {
-        id      => $self->id,
-        zipcode => $self->zipcode,
-        street  => $self->street,
-        company => $self->company,
-        nick    => $self->nick,
-        website => $self->website,
-        INN     => $self->INN,
-        OKPO    => $self->OKPO,
-        OKVED   => $self->OKVED,
-        is_NDS  => $self->is_NDS,
-        is_published => $self->is_published
+        zipcode => $self->get_zipcode,
+        street  => $self->get_street,
+        name    => $self->get_name,
+        nick    => $self->get_nick,
+        website => $self->get_website,
+        INN     => $self->get_INN,
+        OKPO    => $self->get_OKPO,
+        OKVED   => $self->get_OKVED,
+        is_NDS  => $self->get_is_NDS,
+        is_published => $self->get_is_published
     };
 }
 
--- lib/ClubSpain/Model/Role/Company.pm
+++ lib/ClubSpain/Model/Role/Company.pm
@@ -6,38 +6,30 @@ use namespace::autoclean;
 use Moose::Role;
 
 requires qw(
-    zipcode
-    street
-    company
-    nick
-    website
-    INN
-    OKPO
-    OKVED
-    is_NDS
-    validate_zipcode
-    validate_street
-    validate_company
-    validate_nick
-    validate_website
-    validate_INN
-    validate_OKPO
-    validate_OKVED
-    validate_is_NDS
+    get_zipcode set_zipcode validate_zipcode
+    get_street  set_street  validate_street
+    get_name    set_name    validate_name
+    get_nick    set_nick    validate_nick
+    get_website set_website validate_website
+    get_INN     set_INN     validate_INN
+    get_OKPO    set_OKPO    validate_OKPO
+    get_OKVED   set_OKVED   validate_OKVED
+    get_is_NDS  set_is_NDS  validate_is_NDS
+
 );
 
 sub notify {
     my ($self, $object) = @_;
 
-    $self->zipcode($object->zipcode);
-    $self->street($object->street);
-    $self->company($object->company);
-    $self->nick($object->nick);
-    $self->website($object->website);
-    $self->INN($object->INN);
-    $self->OKPO($object->OKPO);
-    $self->OKVED($object->OKVED);
-    $self->is_NDS($object->is_NDS);
+    $self->set_zipcode($object->get_zipcode);
+    $self->set_street($object->get_street);
+    $self->set_name($object->get_name);
+    $self->set_nick($object->get_nick);
+    $self->set_website($object->get_website);
+    $self->set_INN($object->get_INN);
+    $self->set_OKPO($object->get_OKPO);
+    $self->set_OKVED($object->get_OKVED);
+    $self->set_is_NDS($object->get_is_NDS);
 }
 
 1;
