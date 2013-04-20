b62d923f74ad7e1c97a2fc5d0a406333e50ca4be
GermanS <german.semenkov@gmail.com>
2013-04-20 16:05:26 +0400

    cosmetics

--- lib/ClubSpain/XML/VipService/Response/FlightSearch/Segment.pm
+++ lib/ClubSpain/XML/VipService/Response/FlightSearch/Segment.pm
@@ -1,18 +1,19 @@
 package ClubSpain::XML::VipService::Response::FlightSearch::Segment;
+
 use strict;
 use warnings;
 use utf8;
+use namespace::autoclean;
+
+use ClubSpain::Types;
+use ClubSpain::XML::VipService::Response::FlightSearch::Airline;
+use ClubSpain::XML::VipService::Response::FlightSearch::Board;
+use ClubSpain::XML::VipService::Response::FlightSearch::Location;
+use DateTime::Format::Strptime;
+use DateTime;
 
 use Moose;
 use Moose::Util::TypeConstraints;
-use DateTime;
-use ClubSpain::Types;
-use DateTime::Format::Strptime;
-use ClubSpain::XML::VipService::Response::FlightSearch::Location;
-use ClubSpain::XML::VipService::Response::FlightSearch::Airline;
-use ClubSpain::XML::VipService::Response::FlightSearch::Board;
-
-
 
 has 'connected'      => ( is => 'ro' );
 has 'starting'       => ( is => 'ro' );
@@ -33,34 +34,35 @@ around BUILDARGS => sub {
     my $class = shift;
     my %args  = ( @_ == 1 ? %{ $_[0] } : @_ );
 
-    if (exists($args{'locationBegin'})) {
-        $args{'locationBegin'} = ClubSpain::XML::VipService::Response::FlightSearch::Location->new(
-            $args{'locationBegin'}
+    if ( exists( $args{ 'locationBegin' } ) ) {
+        $args{ 'locationBegin' } = ClubSpain::XML::VipService::Response::FlightSearch::Location -> new(
+            $args{ 'locationBegin' }
         );
     }
 
-    if (exists($args{'locationEnd'})) {
-        $args{'locationEnd'} = ClubSpain::XML::VipService::Response::FlightSearch::Location->new(
-            $args{'locationEnd'}
+    if (exists($args{ 'locationEnd' })) {
+        $args{ 'locationEnd' } = ClubSpain::XML::VipService::Response::FlightSearch::Location -> new(
+            $args{ 'locationEnd' }
         );
     }
 
-    if (exists($args{'airline'})) {
-        $args{'airline'} = ClubSpain::XML::VipService::Response::FlightSearch::Airline->new(
-            $args{'airline'}
+    if (exists($args{ 'airline' })) {
+        $args{ 'airline' } = ClubSpain::XML::VipService::Response::FlightSearch::Airline -> new(
+            $args{ 'airline' }
         );
     }
 
-    if (exists($args{'board'})) {
-        $args{'board'} = ClubSpain::XML::VipService::Response::FlightSearch::Board->new(
-            $args{'board'}
+    if (exists($args{ 'board' })) {
+        $args{ 'board' } = ClubSpain::XML::VipService::Response::FlightSearch::Board -> new(
+            $args{ 'board' }
         );
     }
 
-    return $class->$orig(%args);
+    return $class -> $orig( %args );
 };
 
-
-__PACKAGE__->meta->make_immutable();
+__PACKAGE__ -> meta() -> make_immutable();
 
 1;
+
+__END__
