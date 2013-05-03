aabcb8166dff849bbac05db1eb0fa55b487d7068
GermanS <german.semenkov@gmail.com>
2013-05-03 16:02:13 +0400

    add helper for dumping raw data

---
+++ lib/ClubSpain/XML/VipService/Dump.pm
@@ -0,0 +1,30 @@
+package ClubSpain::XML::VipService::Dump;
+
+use namespace::autoclean;
+use strict;
+use warnings;
+use utf8;
+
+use Moose;
+use File::Slurp;
+
+has [ 'airport_of_departure', 'airport_of_arrival' ] => ( is => 'ro', required => 1 );
+has [ 'date_of_departure', 'date_of_arrival' ]       => ( is => 'ro', required => 1 );
+has 'content' => ( is => 'rw' );
+
+sub store {
+    my $self = shift;
+
+    my $filename = sprintf "%s_%s_%s_%s", $self -> airport_of_departure(),
+                                          $self -> airport_of_arrival(),
+                                          $self -> date_of_departure(),
+                                          $self -> date_of_arrival();
+
+    write_file( $filename, $self -> content() );
+}
+
+__PACKAGE__ -> meta() -> make_immutable();
+
+1;
+
+__END__
