e32887d2edb8df7c2aa223ca13badb93a7e0d544
GermanS <german.semenkov@gmail.com>
2013-08-22 18:48:54 +0400

    (Documentation) add pod

--- lib/ClubSpain/Schema/Result/BankAccount.pm
+++ lib/ClubSpain/Schema/Result/BankAccount.pm
@@ -1,13 +1,15 @@
 package ClubSpain::Schema::Result::BankAccount;
+
 use strict;
 use warnings;
 use utf8;
+
 use parent qw(ClubSpain::Schema::Result);
 
-__PACKAGE__->load_components(qw(Core PK::Auto));
-__PACKAGE__->table('bank_account');
-__PACKAGE__->source_name('BankAccount');
-__PACKAGE__->add_columns(
+__PACKAGE__ -> load_components( qw(Core PK::Auto) );
+__PACKAGE__ -> table( 'bank_account' );
+__PACKAGE__ -> source_name( 'BankAccount' );
+__PACKAGE__ -> add_columns(
     id => {
         data_type         => 'integer',
         is_auto_increment => 1
@@ -43,15 +45,69 @@ __PACKAGE__->add_columns(
     },
 );
 
-__PACKAGE__->set_primary_key('id');
-__PACKAGE__->belongs_to(
+__PACKAGE__ -> set_primary_key( 'id' );
+__PACKAGE__ -> belongs_to(
     'company' => 'ClubSpain::Schema::Result::Company', 'company_id'
 );
 
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
 
-    $sqlt_table->add_index(name => 'on_is_published', fields => ['is_published']);
+    $sqlt_table -> add_index( name => 'on_is_published', fields => [ 'is_published' ] );
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+=head1 DESCRIPTION
+=head1 FIELDS
+
+=head2 id
+
+Идентификатор
+
+=head2 company_id 
+
+Идентфикатор компании
+
+=head2 bank
+
+Название банка
+
+=head2 BIC
+
+БИК 
+
+=head2 current_account
+
+Номер расчетного счета
+
+=head2 correspondent_account 
+
+Номер корреспондентского счета
+
+=head2 is_published 
+
+Флаг активности/доступности
+true - активен/доступен
+
+=head1 METHODS
+
+=head2 company
+
+Поучение компании, которой принадлежит банковский счет.
+
+=head1 SEE ALSO
+
+L<DBIx::Class>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
