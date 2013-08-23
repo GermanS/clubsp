4830ffe192b97a3586a7844c3e926b3055ac31f3
GermanS <german.semenkov@gmail.com>
2013-08-23 20:55:53 +0400

    (Documentation) add pod

--- lib/ClubSpain/Model/Airport.pm
+++ lib/ClubSpain/Model/Airport.pm
@@ -1,7 +1,11 @@
 package ClubSpain::Model::Airport;
-use Moose;
-use namespace::autoclean;
+
+use strict;
+use warnings;
 use utf8;
+use namespace::autoclean;
+
+use Moose;
 use parent qw(ClubSpain::Model::Base);
 use ClubSpain::Types;
 
@@ -44,57 +48,113 @@ has 'is_published' => (
 
 with 'ClubSpain::Model::Role::Airport';
 
-sub validate_city_id {
-}
-sub validate_name {
-    my ($self, $value) = @_;
-    $self->meta()->get_attribute('name')->type_constraint->validate($value);
-}
-sub validate_iata {
-    my ($self, $value) = @_;
-    $self->meta()->get_attribute('iata')->type_constraint->validate($value);
-}
-sub validate_icao {
-    my ($self, $value) = @_;
-    $self->meta()->get_attribute('icao')->type_constraint->validate($value);
-}
-
-
-
 sub create {
     my $self = shift;
 
-    $self->SUPER::create( $self->params() );
+    $self -> SUPER::create( $self -> params() );
 }
 
 sub update {
     my $self = shift;
 
-    $self->check_for_class_method();
-    $self->SUPER::update( $self->params() );
-}
-
-sub params {
-    my $self = shift;
-
-    return {
-        iata         => $self->get_iata,
-        icao         => $self->get_icao,
-        name         => $self->get_name,
-        city_id      => $self->get_city_id,
-        is_published => $self->get_is_published,
-    };
+    $self -> check_for_class_method();
+    $self -> SUPER::update( $self -> params() );
 }
 
 sub searchAirportsOfDeparture {
     my ($self, %params) = @_;
 
     return
-        $self->schema()
-             ->resultset($self->source_name)
-             ->searchAirportsOfDeparture(%params);
+        $self -> schema()
+              -> resultset( $self -> source_name() )
+              -> searchAirportsOfDeparture( %params );
 }
 
-__PACKAGE__->meta->make_immutable();
+__PACKAGE__ -> meta() -> make_immutable();
 
 1;
+
+__END__
+
+=head1 NAME
+
+ClubSpian::Model::Airport
+
+=head1 SYNOPSIS
+
+    use ClubSpain::Model::Airport;
+    my $object = ClubSpiain::Model::Airport -> new(
+        id           => $id,
+        name         => $name,
+        iata         => $iata,
+        icao         => $icao,
+        city_id      => $city_id,
+        is_published => $flag,
+    );
+
+    if( $object -> validate_name() ) { ... }
+    if( $object -> validate_iata() ) { ... }
+    if( $object -> validate_icao() ) { ... }
+    if( $object -> validate_city_id() ) { ... }
+
+    my $res = $object -> create();
+    my $res = $object -> update();
+    my $res = $object -> delete();
+    my $iterator = $object -> searchAirportsOfDeparture( %params )
+
+=head1 DESCRIPTION
+
+Аэропорт
+
+=head1 FIELDS
+
+=head2 id
+
+Идентификатор аэропорта
+
+=head2 city_id
+
+Идентификатор города
+
+=head2 iata
+
+IATA код аэропорта
+
+=head2 icao
+
+ICAO код аэропорта
+
+=head2 name
+
+Название аэропорта
+
+=head2 is_published
+
+Флаг опубликованности
+
+=head1 METHODS
+
+=head1 create()
+
+Добавление аэропорта в дазу данных
+
+=head1 update()
+
+Обновление информации об аэропорте
+
+=head1 searchAirportsOfDeparture(%params)
+
+Поиск аэропортов отправления по заданным критериям
+
+=head1 SEE ALSO
+
+L<ClubSpain::Model::Role::Airport>
+L<ClubSpian::Model::Base>
+L<Moose>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
--- lib/ClubSpain/Model/Role/Airport.pm
+++ lib/ClubSpain/Model/Role/Airport.pm
@@ -1,23 +1,109 @@
 package ClubSpain::Model::Role::Airport;
+
 use strict;
 use warnings;
 use utf8;
 use namespace::autoclean;
+
 use Moose::Role;
 
 requires qw(
-    get_city_id set_city_id validate_city_id
-    get_name    set_name    validate_name
-    get_iata    set_iata    validate_iata
-    get_icao    set_icao    validate_icao
+    get_city_id set_city_id
+    get_name    set_name
+    get_iata    set_iata
+    get_icao    set_icao
 );
 
 sub notify {
     my ($self, $object) = @_;
-    $self->set_city_id($object->get_city_id);
-    $self->set_name($object->get_name);
-    $self->set_iata($object->get_iata);
-    $self->set_icao($object->get_icao);
+
+    $self -> set_city_id( $object -> get_city_id() );
+    $self -> set_name( $object -> get_name() );
+    $self -> set_iata( $object -> get_iata() );
+    $self -> set_icao( $object -> get_icao() );
+
+    return;
+}
+
+sub validate_city_id {
+}
+
+sub validate_name {
+    my ($self, $value) = @_;
+
+    return
+        $self -> meta() -> get_attribute('name') -> type_constraint() -> validate( $value );
+}
+
+sub validate_iata {
+    my ($self, $value) = @_;
+
+    return
+        $self -> meta() -> get_attribute('iata') -> type_constraint() -> validate( $value );
+}
+
+sub validate_icao {
+    my ($self, $value) = @_;
+
+    return
+        $self -> meta() -> get_attribute('icao') -> type_constraint() -> validate( $value );
+}
+
+sub params {
+    my $self = shift;
+
+    return {
+        iata         => $self -> get_iata(),
+        icao         => $self -> get_icao(),
+        name         => $self -> get_name(),
+        city_id      => $self -> get_city_id(),
+        is_published => $self -> get_is_published(),
+    };
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+ClubSpain::Model::Role::Airport
+
+=head1 DESCTIPTION
+
+Интерфейс для аэропорта
+
+=head1 METHODS
+
+=head2 validate_city_id()
+
+Проверка идентификатора города
+
+=head2 validate_name()
+
+Проверкам названия аэропорта
+
+=head2 validate_icao()
+
+Проверка ICAO кода аэропорта
+
+=head2 validate_iata()
+
+Проверка IATA кода аэропорта
+
+=head2 params()
+
+#TODO: документация для params()
+
+=head1 SEE ALSO
+
+L<Moose::Role>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
