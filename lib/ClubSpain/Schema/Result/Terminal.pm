304b3ccf8b3b177c1345accb1925bf93b8af1919
GermanS <german.semenkov@gmail.com>
2013-08-23 16:13:47 +0400

    (Documentation) add pod

--- lib/ClubSpain/Schema/Result/Terminal.pm
+++ lib/ClubSpain/Schema/Result/Terminal.pm
@@ -1,13 +1,15 @@
 package ClubSpain::Schema::Result::Terminal;
+
 use strict;
 use warnings;
 use utf8;
+
 use parent qw(ClubSpain::Schema::Result);
 
-__PACKAGE__->load_components(qw(Core PK::Auto));
-__PACKAGE__->table('terminal');
-__PACKAGE__->source_name('Terminal');
-__PACKAGE__->add_columns(
+__PACKAGE__ -> load_components(qw(Core PK::Auto));
+__PACKAGE__ -> table('terminal');
+__PACKAGE__ -> source_name('Terminal');
+__PACKAGE__ -> add_columns(
     id => {
         data_type         => 'integer',
         is_auto_increment => 1
@@ -18,29 +20,84 @@ __PACKAGE__->add_columns(
         is_foreign_key => 1,
     },
     name => {
-        data_type     => 'char',
-        size          => 255,
-        is_nullable   => 0,
+        data_type   => 'char',
+        size        => 255,
+        is_nullable => 0,
     },
     is_published => {
-        data_type     => 'TINYINT(1) UNSIGNED',
-        is_nullable    => 0,
+        data_type   => 'TINYINT(1) UNSIGNED',
+        is_nullable => 0,
     },
 );
 
-__PACKAGE__->set_primary_key('id');
+__PACKAGE__ -> set_primary_key('id');
 
-__PACKAGE__->has_many(
+__PACKAGE__ -> has_many(
     departures => 'ClubSpain::Schema::Result::TimeTable', {'foreign.departure_terminal_id' => 'self.id'}
 );
-__PACKAGE__->has_many(
+__PACKAGE__ -> has_many(
     arrivals   => 'ClubSpain::Schema::Result::TimeTable', {'foreign.arrival_terminal_id' => 'self.id'}
 );
 
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
 
-    $sqlt_table->add_index(name => 'on_is_published', fields => ['is_published']);
+    $sqlt_table -> add_index(
+        name => 'on_is_published',
+        fields => ['is_published']
+    );
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+ClubSpain::Schema::Result::Terminal
+
+=head1 DESCRIPTION
+
+Модель таблицы terminal - терминал аэропорта
+
+=head1 FIELDS
+
+=head2 id
+
+Идентификатор терминала
+
+=head2 airport_id
+
+Идентификатор аэропорта
+
+=head2 name
+
+Название терминала
+
+=head2 is_published
+
+Флаг опубликованности терминала аэропорта
+
+=head1 METHODS
+
+=head2 departures()
+
+Получение списка расписаний рейсов на отправлении из терминала
+
+=head2 arrivals()
+
+Получение списка расписаний рейсов на прибытие в терминал
+
+=head1 SEE ALSO
+
+L<ClubSpain::Schema::Result::TimeTable>
+L<DBix::Class>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
