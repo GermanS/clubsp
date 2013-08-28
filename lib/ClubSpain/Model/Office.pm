c8967948b45f0cca9e9e0d0ffc90aa91968b955c
GermanS <german.semenkov@gmail.com>
2013-08-28 15:06:26 +0400

    (Documentation) add pod

--- lib/ClubSpain/Model/Office.pm
+++ lib/ClubSpain/Model/Office.pm
@@ -1,7 +1,11 @@
 package ClubSpain::Model::Office;
-use Moose;
-use namespace::autoclean;
+
+use strict;
+use warnings;
 use utf8;
+use namespace::autoclean;
+
+use Moose;
 use parent qw(ClubSpain::Model::Base);
 use ClubSpain::Types;
 
@@ -44,28 +48,106 @@ has 'is_published' => (
 sub create {
     my $self = shift;
 
-    $self->SUPER::create( $self->params );
+    $self -> SUPER::create( $self -> params );
 }
 
 sub update {
     my $self = shift;
 
-    $self->check_for_class_method();
-    $self->SUPER::update( $self->params() );
+    $self -> check_for_class_method();
+    $self -> SUPER::update( $self -> params() );
 }
 
 sub params {
     my $self = shift;
 
     return {
-        company_id  => $self->get_company_id,
-        zipcode     => $self->get_zipcode,
-        street      => $self->get_street,
-        name        => $self->get_name,
-        is_published => $self->get_is_published
+        company_id  => $self -> get_company_id(),
+        zipcode     => $self -> get_zipcode(),
+        street      => $self -> get_street(),
+        name        => $self -> get_name(),
+        is_published => $self -> get_is_published()
     };
 }
 
-__PACKAGE__->meta()->make_immutable;
+__PACKAGE__ -> meta() -> make_immutable;
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+ClubSpain::Model::Office
+
+=head1 SYNOPSIS
+
+use ClubSpain::Model::Office;
+my $object = ClubSpain::Model::Office -> new(
+    id           => $id,
+    company_id   => $company,
+    zipcode      => $zipcode,
+    street       => $street,
+    name         => $name,
+    is_published => $flag,
+);
+
+my $res = $object -> create();
+my $res = $object -> update();
+
+=head1 DESCRIPTION
+
+Офис компании(предприятия)
+
+=head1 FIELDS
+
+=head2 id
+
+Идентификатор офиса
+
+=head2 company_id
+
+Иденфтификатор компании
+
+=head2 zipcode
+
+Почтовый индекс офиса
+
+=head2 street
+
+Адрес
+
+=head2 name
+
+Название офиса
+
+=head2 is_published
+
+Флаг опубликованности
+
+=head1 METHODS
+
+=head2 create()
+
+Добавление записи в базу данных
+
+=head2 update()
+
+Редактирование записи в базе данных
+
+=head2 params()
+
+#TODO: документация для params()
+
+=head1 SEE ALSO
+
+L<Moose>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
