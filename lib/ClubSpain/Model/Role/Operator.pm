9d774920406a80d3e130fc1b712ecf06d442fc1e
GermanS <german.semenkov@gmail.com>
2013-08-28 15:29:43 +0400

    (Documentation) add pod

--- lib/ClubSpain/Model/Role/Operator.pm
+++ lib/ClubSpain/Model/Role/Operator.pm
@@ -30,3 +30,54 @@ sub params {
 1;
 
 __END__
+
+=pod
+
+=head1 NAME
+
+ClubSpain::Model::Role::Operator;
+
+=head1 DESCRIPTION
+
+Интерфейсные методя для работы с сотрудников компании
+
+=head1 METHODS
+
+=head2 validate_office_id()
+
+Проверка идентификатор а офиса
+
+=head2 validate_name()
+
+Проверка имени
+
+=head2 validate_surname()
+
+Проверка фамилии
+
+=head2 validate_email()
+
+Проверка адреса электронной почты
+
+=head2 validate_passwd()
+
+Проверка пароля
+
+=head2 validate_mobile()
+
+Проверка номера мобильного телефона
+
+=head2 params()
+
+#TODO: документация params()
+
+=head1 SEE ALSO
+
+L<Moose::Role>
+
+=head1 AUTHOR
+
+German Semenkov
+german.semenkov@gmail.com
+
+=cut
